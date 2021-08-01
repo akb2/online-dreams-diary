877f3a1c832513ae1e9bff621e4423a865a86046
akb2 <akb2-online@ya.ru>
2021-08-01 14:16:13 +0300

    Настроил роутинг страниц

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,36 +1,62 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { AuthGuard } from "@_helpers/auth-guard";
 
 
 
 
 
 const routes: Routes = [
+  // ! Информация о ключе authRule см. в типе AuthRules файла menu.ts в моделях
+  // * Для неавторизованных пользователей
   // Главная страница
   {
-    path: "cube",
-    loadChildren: () => import("@_pages/cube/cube.module").then(m => m.CubeModule)
-  },
-  // Главная страница
-  {
-    path: "",
-    loadChildren: () => import("@_pages/home/home.module").then(m => m.HomeModule)
+    path: "home",
+    loadChildren: () => import("@_pages/home/home.module").then(m => m.HomeModule),
+    data: { authRule: -1 },
+    canActivate: [AuthGuard]
   },
   // Авторизация
   {
     path: "auth",
-    loadChildren: () => import("@_pages/auth/auth.module").then(m => m.AuthModule)
+    loadChildren: () => import("@_pages/auth/auth.module").then(m => m.AuthModule),
+    data: { authRule: -1 },
+    canActivate: [AuthGuard]
   },
   // Регистрация
   {
     path: "register",
-    loadChildren: () => import("@_pages/register/register.module").then(m => m.RegisterModule)
+    loadChildren: () => import("@_pages/register/register.module").then(m => m.RegisterModule),
+    data: { authRule: -1 },
+    canActivate: [AuthGuard]
+  },
+  // * Для авторизованных пользователей
+  // Личный кабинет
+  {
+    path: "cabinet",
+    loadChildren: () => import("@_pages/cabinet/cabinet.module").then(m => m.CabinetModule),
+    data: { authRule: 1 },
+    canActivate: [AuthGuard]
+  },
+  // * Страницы, не имеющие значения авторизации
+  // Главная страница
+  {
+    path: "",
+    children: [],
+    data: {
+      authRule: 0,
+      redirectAuth: "/cabinet",
+      redirectNotAuth: "/home",
+    },
+    canActivate: [AuthGuard]
   },
   // Ошибка 404
   {
     path: "**",
-    loadChildren: () => import("@_pages/404/404.module").then(m => m.Page404Module)
-  }
+    loadChildren: () => import("@_pages/404/404.module").then(m => m.Page404Module),
+    data: { authRule: 0 },
+    canActivate: [AuthGuard]
+  },
 ];
 
 
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { AccountService } from '@_services/account.service';
+import { ApiService } from '@_services/api.service';
+import { SnackbarService } from '@_services/snackbar.service';
 
 
 
@@ -21,6 +24,7 @@ export class AppComponent {
   public title: string = 'app';
 
   public showPreloader: boolean = true;
+  public validToken: boolean = false;
   private loaderDelay: number = 150;
 
 
@@ -29,8 +33,35 @@ export class AppComponent {
 
   // Конструктор
   constructor(
-    private router: Router
+    private router: Router,
+    private accountService: AccountService,
+    private snackBar: SnackbarService,
+    private apiService: ApiService
   ) {
+    // Пользователь авторизован, проверить токен
+    if (this.accountService.checkAuth) {
+      this.accountService.checkToken(["9014", "9015", "9016"]).subscribe(code => {
+        // Если токен валидный
+        if (code == "0001") {
+          this.validToken = true;
+        }
+        // Токен не валидный
+        else {
+          this.router.navigate([""]);
+          this.accountService.deleteAuth();
+          // Сообщение с ошибкой
+          this.snackBar.open({
+            "mode": "error",
+            "message": this.apiService.getMessageByCode(code)
+          });
+        }
+      });
+    }
+    // Пользователь неавторизован
+    else {
+      this.validToken = true;
+    }
+    // События старта и окочания лоадера
     this.router.events.subscribe(event => {
       if (event instanceof NavigationStart) {
         this.beforeLoadPage();
@@ -48,8 +79,11 @@ export class AppComponent {
 
   // Действия перед загрузкой страницы
   private beforeLoadPage(): void {
-    this.showPreloader = true;
-    document.querySelectorAll("body, html").forEach(elm => elm.classList.add("no-scroll"));
+    const showPreloader: boolean = !(this.router.getCurrentNavigation()?.extras.state?.showPreLoader == false);
+    if (showPreloader) {
+      this.showPreloader = true;
+      document.querySelectorAll("body, html").forEach(elm => elm.classList.add("no-scroll"));
+    }
   }
 
   // Действия после загрузки страницы
