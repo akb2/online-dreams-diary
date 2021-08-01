fdbe11d6fa5714f2862d7e28ff7b07c0dab72b39
akb2 <akb2-online@ya.ru>
2021-08-01 14:06:33 +0300

    Добавил модель для меню и роутинга

---
+++ client/src/app/models/menu.ts
@@ -0,0 +1,16 @@
+// Элемент главного меню
+export interface MenuItem {
+  auth: AuthRules;
+  icon: string;
+  text: string;
+  callback?: () => void;
+  link?: string;
+  linkParams?: { [key: string]: string };
+}
+
+// Тип способа авторизации
+// * Ключ auth отвечает за показ пунктов:
+// * -1: Только неавторизованным
+// * 0: Всегда
+// * 1: Только авторизованным
+export type AuthRules = -1 | 0 | 1;
