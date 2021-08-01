9a8599c5284d0f7d6bb89f2fae81569b14247611
akb2 <akb2-online@ya.ru>
2021-08-01 03:20:01 +0300

    Лоадер и вывод сообщений

---
+++ client/src/app/controlers/elements/inform/inform.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from "@angular/core";
+
+
+
+
+
+@Component({
+  selector: "app-inform",
+  templateUrl: "./inform.component.html",
+  styleUrls: ["./inform.component.scss"]
+})
+
+
+
+
+
+export class InformComponent {
+  @Input() public icon: string = "loader";
+  @Input() public title: string;
+  @Input() public subTitle: string;
+  @Input() public description: string;
+}
---
+++ client/src/app/controlers/elements/inform/inform.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { InformComponent } from "./inform.component";
+
+
+
+
+
+@NgModule({
+  declarations: [
+    InformComponent
+  ],
+  exports: [
+    InformComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+
+
+
+
+
+export class InformModule { }
