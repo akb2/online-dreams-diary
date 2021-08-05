5b15a64239210a76860a0852d560e050adad5c4d
akb2 <akb2-online@ya.ru>
2021-08-06 01:36:40 +0300

    Перенес логику в сервис, добавил запрос юзера

--- api/controllers/account.php
+++ api/controllers/account.php
@@ -4,12 +4,13 @@ namespace OnlineDreamsDiary\Controllers;
 
 include_once "services/recaptcha.php";
 include_once "services/database.php";
+include_once "services/user.php";
 include_once "config/database.php";
 
 use OnlineDreamsDiary\Services\ReCaptchaService;
 use OnlineDreamsDiary\Services\DataBaseService;
+use OnlineDreamsDiary\Services\UserService;
 use PDO;
-use PDOException;
 
 
 
@@ -18,6 +19,10 @@ class Account
 
   private array $config;
   private PDO $pdo;
+
+  private UserService $userService;
+
+
 
   // Получить настройки БД
   public function setDbContext(PDO $pdo): void
@@ -31,262 +36,54 @@ class Account
     $this->config = $config;
   }
 
+  // Запуск сервисов
+  public function setServices(): void
+  {
+    $this->userService = new UserService($this->pdo, $this->config);
+  }
+
 
 
   // Создание таблиц
   // * POST
-  public function createTable($data)
+  public function createTable($data): array
   {
-    $result = array(
-      "deleteTable" => false,
-      "deleteTokensTable" => false,
-      "createTable" => false,
-      "createTokensTable" => false,
-      "createAdmin" => false,
-    );
-    // Проверить секретный пароль
-    if ($data["password"] == $this->config["appPassword"]) {
-      $dataBase = new DataBaseService($this->pdo);
-      // Настройка таблиц
-      $result["deleteTable"] = $dataBase->executeFromFile("account/deleteTable.sql");
-      $result["deleteTokensTable"] = $dataBase->executeFromFile("account/deleteTokensTable.sql");
-      $result["createTable"] = $dataBase->executeFromFile("account/createTable.sql");
-      $result["createTokensTable"] = $dataBase->executeFromFile("account/createTokensTable.sql");
-      $result["createAdmin"] = $dataBase->executeFromFile("account/createAdmin.sql");
-    }
-    // Результат работы функции
-    return $result;
+    return $this->userService->createTableApi($data["password"]);
   }
 
   // Проверка токена
   // * POST
-  public function checkToken($data)
+  public function checkToken($data): array
   {
-    $code = "0000";
-    $message = "";
-    $sqlData = array();
-    $tokenData = array();
-    $tokenLifeTime = $this->config["auth"]["tokenLifeTime"] ? $this->config["auth"]["tokenLifeTime"] : 36000;
-
-    // Если получены данные
-    if (strlen($data["token"]) > 0) {
-      $dataBase = new DataBaseService($this->pdo);
-      $code = "9015";
-      // Данные
-      $sqlData = array($data["token"]);
-      // Запрос проверки токена
-      $auth = $dataBase->getDatasFromFile("account/checkToken.sql", $sqlData);
-      // Проверить авторизацию
-      if (count($auth) > 0) {
-        if (strlen($auth[0]["id"]) > 0) {
-          // Проверка времени жизни токена
-          if (gmdate("U") - $tokenLifeTime < strtotime($auth[0]["last_action_date"])) {
-            // Обновить токен
-            if ($dataBase->executeFromFile("account/updateTokenLastAction.sql", array($auth[0]["id"]))) {
-              $tokenData = $auth[0];
-              $code = "0001";
-            }
-            // Ошибка сохранения токена
-            else {
-              $code = "9014";
-            }
-          }
-          // Токен просрочен
-          else {
-            $code = "9016";
-          }
-        }
-      }
-
-      // Удалить токен
-      if ($code != "0001") {
-        $dataBase->executeFromFile("account/deleteToken.sql", array($data["token"]));
-      }
-    }
-    // Получены пустые данные
-    else {
-      $code = "9030";
-    }
-
-    // Вернуть массив
-    return array(
-      "code" => $code,
-      "message" => $message,
-      "data" => array(
-        "input" => $sqlData,
-        "tokenData" => $tokenData,
-        "result" => $code == "0001"
-      )
-    );
+    return $this->userService->checkTokenApi($data);
   }
 
   // Удалить токен
   // * DELETE
-  public function deleteToken($data)
+  public function deleteToken($data): array
   {
-    $code = "0000";
-    $message = "";
-    $sqlData = array();
-
-    // Если получены данные
-    if (strlen($data["token"]) > 0) {
-      $dataBase = new DataBaseService($this->pdo);
-      // Удаление токена
-      if ($dataBase->executeFromFile("account/deleteToken.sql", array($data["token"]))) {
-        $code = "0001";
-      }
-      // Неудалось удалить токен
-      else {
-        $code = "9017";
-      }
-    }
-    // Получены пустые данные
-    else {
-      $code = "9030";
-    }
-
-    // Вернуть массив
-    return array(
-      "code" => $code,
-      "message" => $message,
-      "data" => array(
-        "input" => $sqlData,
-        "result" => $code == "0001"
-      )
-    );
+    return $this->userService->deleteTokenApi($data["token"]);
   }
 
   // Авторизация пользователя
   // * POST
-  public function auth($data)
+  public function auth($data): array
   {
-    $code = "9010";
-    $message = "";
-    $id = "";
-    $token = "";
-    $sqlData = array();
-
-    // Если получены данные
-    if (strlen($data["login"]) > 0 && strlen($data["password"]) > 0) {
-      $dataBase = new DataBaseService($this->pdo);
-      $code = "9013";
-      // Данные
-      $sqlData = array(
-        $data["login"],
-        hash("sha512", $this->config["hashSecret"] . $data["password"])
-      );
-      // Запрос авторизации
-      $auth = $dataBase->getDatasFromFile("account/auth.sql", $sqlData);
-      // Проверить авторизацию
-      if (count($auth) > 0) {
-        if (strlen($auth[0]["id"]) > 0) {
-          $id = $auth[0]["id"];
-          $token = hash("sha512", $this->config["hashSecret"] . $id . "_" . gmdate("U"));
-          // Сохранить токен
-          if ($dataBase->executeFromFile("account/createToken.sql", array($token, $id))) {
-            $code = "0001";
-          }
-          // Ошибка сохранения токена
-          else {
-            $code = "9014";
-          }
-        }
-      }
-      // Убрать метку пароля
-      //unset($sqlData[1]);
-    }
-    // Получены пустые данные
-    else {
-      $code = "9030";
-    }
-
-    // Вернуть массив
-    return array(
-      "code" => $code,
-      "message" => $message,
-      "data" => array(
-        "id" => $id,
-        "token" => $token,
-        "input" => $sqlData,
-        "result" => $code == "0001"
-      )
-    );
+    return $this->userService->authUserApi($data);
   }
 
   // Регистрация пользователя
   // * POST
-  public function register($data)
+  public function register($data): array
   {
-    $code = "9010";
-    $message = "";
-    $id = "";
-    $sqlData = array();
-    // Если получены данные
-    if (strlen($data["login"]) > 0 && strlen($data["email"]) > 0) {
-      $dataBase = new DataBaseService($this->pdo);
-      $reCaptcha = new ReCaptchaService($data["captcha"], $this->config);
-      // Данные
-      $sqlData = array(
-        $data["login"],
-        hash("sha512", $this->config["hashSecret"] . $data["password"]),
-        $data["name"],
-        $data["lastName"],
-        date("Y-m-d", strtotime($data["birthDate"])),
-        $data["sex"],
-        $data["email"],
-        json_encode(array())
-      );
+    return $this->userService->registerUserApi($data);
+  }
 
-      // Проверить капчу
-      if ($reCaptcha->checkCaptcha()) {
-        $checkLogin = $dataBase->getDatasFromFile("account/checkLoginEmail.sql", array($data["login"], $data["email"]));
-        // Проверить логин
-        if (count($checkLogin) == 0) {
-          // Регистрация пользователя
-          if ($dataBase->executeFromFile("account/registerUser.sql", $sqlData)) {
-            // TODO: Добавить отправку письма на почту
-            $code = "0001";
-          }
-          // Регистрация неудалась
-          else {
-            $code = "9021";
-          }
-        }
-        // Логин или почта повторяются
-        else {
-          foreach ($checkLogin as $row) {
-            // Логин
-            if ($row["login"] == $data["login"]) {
-              $code = "9011";
-            }
-            // Почта
-            else {
-              $code = "9012";
-            }
-            break;
-          }
-        }
-      }
-      // Ошибка капчи
-      else {
-        $code = "9010";
-      }
-    }
-    // Получены пустые данные
-    else {
-      $code = "9030";
-    }
-
-    // Вернуть массив
-    return array(
-      "code" => $code,
-      "message" => $message,
-      "data" => array(
-        "id" => $id,
-        "input" => $sqlData,
-        "result" => $code == "0001"
-      )
-    );
+  // Получить данные о пользователе
+  // * GET
+  public function getUser($data): array
+  {
+    return $this->userService->getUserApi($data["id"]);
   }
 }
 
