6979116bef620e3a19cb47290eb6950eb2a8ec7f
akb2 <akb2-online@ya.ru>
2021-08-08 19:43:29 +0300

    Форматирование

--- client/src/app/controlers/elements/toggle-input/toggle-input.component.ts
+++ client/src/app/controlers/elements/toggle-input/toggle-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 import { MatFormFieldAppearance } from "@angular/material/form-field";
 import { BaseInputDirective } from "@app/directives/base-input.directive";
 
@@ -6,11 +6,14 @@ import { BaseInputDirective } from "@app/directives/base-input.directive";
 
 
 
+// Декоратор
 @Component({
   selector: "app-toggle-input",
   templateUrl: "./toggle-input.component.html",
   styleUrls: ["./toggle-input.component.scss"]
 })
+
+// Класс
 export class ToggleInputComponent extends BaseInputDirective {
   @Input() public label: string = "Заголовок";
   @Input() public values: [string, string] = ["Параметр 1", "Параметр 2"];
