4c40153d98a6fc6052ef6a6b898705ed1ea30d55
akb2 <akb2-online@ya.ru>
2021-08-09 00:35:25 +0300

    Добавил модули

--- client/src/app/pages/profile/profile.module.ts
+++ client/src/app/pages/profile/profile.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { CardModule } from '@_controlers/card/card.module';
 import { ContentMenuModule } from '@_controlers/content-menu/content-menu.module';
+import { ImageUploadModule } from '@_controlers/image-upload/image-upload.module';
 import { NavMenuModule } from "@_controlers/nav-menu/nav-menu.module";
 import { TextInputModule } from '@_controlers/text-input/text-input.module';
 import { ToggleInputModule } from '@_controlers/toggle-input/toggle-input.module';
@@ -17,8 +19,7 @@ import { SettingsProfileComponent } from '@_pages/profile/settings/settings.comp
 
 
 
-
-
+// Декоратор
 @NgModule({
   declarations: [
     ProfileComponent,
@@ -36,7 +37,11 @@ import { SettingsProfileComponent } from '@_pages/profile/settings/settings.comp
     TextInputModule,
     ToggleInputModule,
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    ImageUploadModule,
+    MatButtonModule
   ]
 })
+
+// Модуль
 export class ProfileModule { }
