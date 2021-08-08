e0293bfcdfd7c4954bb71c98f30a2bda1500caaa
akb2 <akb2-online@ya.ru>
2021-08-08 19:44:07 +0300

    Вывел значения пользователя

--- client/src/app/pages/profile/settings/person/settings-person.component.ts
+++ client/src/app/pages/profile/settings/person/settings-person.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { AppComponent } from '@app/app.component';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { User } from '@_models/account';
-import { ValidatorData } from '@_models/form';
+import { ErrorMessages, ErrorMessagesType, FormData, FormDataType, ValidatorData } from '@_models/form';
+import { AccountService } from '@_services/account.service';
 
 
 
@@ -20,19 +20,50 @@ export class SettingsPersonProfileComponent {
 
 
   public form: FormGroup;
-
-  public get user(): User {
-    return AppComponent.user;
-  };
+  public errors: ErrorMessagesType = ErrorMessages;
+  public formData: FormDataType = FormData;
+  public user: User;
 
   constructor(
+    private accountService: AccountService,
     private formBuilder: FormBuilder
   ) {
     // Настройка формы
-    this.form = formBuilder.group([
-      new FormControl(["name", ValidatorData.name]),
-      new FormControl(["lastName", ValidatorData.name]),
-      new FormControl(["patronymic", ValidatorData.name])
-    ]);
+    this.form = this.formBuilder.group({
+      name: ["", ValidatorData.name],
+      lastName: ["", ValidatorData.name],
+      patronymic: ["", ValidatorData.name],
+      birthDate: ["", ValidatorData.birthDate],
+      sex: [false]
+    });
+    // Подписка на данные пользвателя
+    this.accountService.user$.subscribe(user => {
+      this.user = user;
+      // Значения формы
+      if (this.user) {
+        this.form.get("name").setValue(this.user.name);
+        this.form.get("lastName").setValue(this.user.lastName);
+        this.form.get("patronymic").setValue(this.user.patronymic);
+        this.form.get("birthDate").setValue(new Date(this.user.birthDate));
+        this.form.get("sex").setValue(this.user.sex == 1 ? true : false);
+      }
+    });
+  }
+
+
+
+
+
+  // Сохранение данных
+  public onSaveData(): void {
+  }
+
+
+
+
+
+  // Возраст до даты
+  public ageToDate(age: number): Date {
+    return new Date(Date.now() - (age * 365 * 24 * 60 * 60 * 1000));
   }
 }
