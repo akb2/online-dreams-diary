659b1b6f460e2467b8f47f280aa1aca27f79e45b
akb2 <akb2-online@ya.ru>
2021-08-09 21:40:03 +0300

    Добавил отображение выбранного файла, кнопки

--- client/src/app/controlers/elements/image-upload/image-upload.component.ts
+++ client/src/app/controlers/elements/image-upload/image-upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { MatFormFieldAppearance } from "@angular/material/form-field";
 import { BaseInputDirective } from "@_directives/base-input.directive";
 
@@ -15,6 +15,53 @@ import { BaseInputDirective } from "@_directives/base-input.directive";
 
 // Класс
 export class ImageUploadComponent extends BaseInputDirective {
+
+
   @Input() public label: string;
   @Input() public appearance: MatFormFieldAppearance = "fill";
+
+  @Output() public upload: EventEmitter<File> = new EventEmitter<File>();
+
+  public newValue: string;
+
+  public get imageUrl(): string {
+    if (this.newValue?.length > 0) {
+      return this.newValue;
+    }
+    // Показать изначальное значение
+    else if (typeof this.control.value === "string" && this.control.value?.length > 0) {
+      return this.control.value;
+    }
+    // Нет картинки
+    return "";
+  }
+
+  public get availSave(): boolean {
+    return this.newValue?.length > 0;
+  }
+
+
+
+
+
+  // Выбор файла
+  public onSelectFiles(files: FileList): void {
+    if (files?.length > 0) {
+      const file: File = files[0];
+      const fileReader: FileReader = new FileReader();
+      // Установить новую временную картинку
+      fileReader.readAsDataURL(file);
+      fileReader.onload = () => {
+        this.control.setValue(file);
+        this.newValue = fileReader.result as string;
+      }
+    }
+  }
+
+  // Загрузка файла
+  public onUpload(): void {
+    if (this.control.value && typeof this.control.value === "object") {
+      this.upload.emit(this.control.value);
+    }
+  }
 }
