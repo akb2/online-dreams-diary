5348d79454988a529c787d968df65169101ecaed
akb2 <akb2-online@ya.ru>
2021-08-10 21:57:18 +0300

    Проверка размера и типа файла

--- client/src/app/controlers/elements/image-upload/image-upload.component.ts
+++ client/src/app/controlers/elements/image-upload/image-upload.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { NgControl } from "@angular/forms";
 import { MatFormFieldAppearance } from "@angular/material/form-field";
 import { BaseInputDirective } from "@_directives/base-input.directive";
+import { SnackbarService } from "@_services/snackbar.service";
 
 
 
@@ -14,15 +16,18 @@ import { BaseInputDirective } from "@_directives/base-input.directive";
 })
 
 // Класс
-export class ImageUploadComponent extends BaseInputDirective {
+export class ImageUploadComponent extends BaseInputDirective implements OnInit {
 
 
   @Input() public label: string;
   @Input() public appearance: MatFormFieldAppearance = "fill";
+  @Input() public fileTypes: FileTypes[] = FileTypesDefault;
+  @Input() public fileSize: number = 10485760;
 
   @Output() public upload: EventEmitter<File> = new EventEmitter<File>();
 
   public newValue: string;
+  private defaultValue: string = "";
 
   public get imageUrl(): string {
     if (this.newValue?.length > 0) {
@@ -40,9 +45,21 @@ export class ImageUploadComponent extends BaseInputDirective {
     return this.newValue?.length > 0;
   }
 
+  constructor(
+    public ngControl: NgControl,
+    private snackbarService: SnackbarService
+  ) {
+    super(ngControl);
+  }
 
 
 
+
+
+  // Запуск класса
+  public ngOnInit(): void {
+    this.defaultValue = this.control.value;
+  }
 
   // Выбор файла
   public onSelectFiles(files: FileList): void {
@@ -50,11 +67,31 @@ export class ImageUploadComponent extends BaseInputDirective {
       const file: File = files[0];
       const fileReader: FileReader = new FileReader();
       // Установить новую временную картинку
-      fileReader.readAsDataURL(file);
-      fileReader.onload = () => {
-        this.control.setValue(file);
-        this.newValue = fileReader.result as string;
+      if (file.size <= this.fileSize) {
+        fileReader.readAsDataURL(file);
+        fileReader.onload = () => {
+          this.control.setValue(file);
+          this.newValue = fileReader.result as string;
+        }
       }
+      // Сбросить форму
+      else {
+        this.clearInput();
+        // Сообщение
+        this.snackbarService.open({
+          message: "Превышен допустимый размер файла в " + this.convertSize(this.fileSize),
+          mode: "error"
+        });
+      }
+    }
+    // Сбросить форму
+    else {
+      this.clearInput();
+      // Сообщение
+      this.snackbarService.open({
+        message: "Ошибка выбора файла",
+        mode: "error"
+      });
     }
   }
 
@@ -64,4 +101,40 @@ export class ImageUploadComponent extends BaseInputDirective {
       this.upload.emit(this.control.value);
     }
   }
+
+
+
+
+
+  // Сбросить значение
+  public clearInput(): void {
+    this.control.setValue(this.defaultValue);
+    this.newValue = "";
+  }
+
+  // Получить размер файла
+  private convertSize(size: number): string {
+    const strings: string[] = ["Б", "КБ", "МБ", "ГБ", "ТБ"];
+    let key: number = 0;
+    // Преобразовать данные
+    while (size > 1024 && key < strings.length) {
+      size = size / 1024;
+      key++;
+    }
+    // Преобразование неудалось
+    return Math.round(size) + " " + strings[key];
+  }
 }
+
+
+
+
+
+// Допустимые типы файлов
+type FileTypes = "image/gif" | "image/jpeg" | "image/png";
+
+// Типы файлов по умолчанию
+export const FileTypesDefault: FileTypes[] = [
+  "image/jpeg",
+  "image/png"
+];
