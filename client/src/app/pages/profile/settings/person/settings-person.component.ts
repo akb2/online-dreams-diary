4ef316add24e212d4e76e376053d3093c3c2e9f5
akb2 <akb2-online@ya.ru>
2021-08-19 02:15:28 +0300

    Окончил страницу настроек

--- client/src/app/pages/profile/settings/person/settings-person.component.ts
+++ client/src/app/pages/profile/settings/person/settings-person.component.ts
@@ -1,12 +1,16 @@
 import { formatDate } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { PopupCropImageComponent, PopupCropImageData } from '@_controlers/crop-image/crop-image.component';
 import { ImageUploadComponent } from '@_controlers/image-upload/image-upload.component';
 import { CustomValidators } from '@_helpers/custom-validators';
-import { User, UserSave } from '@_models/account';
+import { User, UserAvatarCropDataElement, UserAvatarCropDataKeys, UserSave } from '@_models/account';
 import { ErrorMessages, ErrorMessagesType, FormData, FormDataType, ValidatorData } from '@_models/form';
 import { AccountService } from '@_services/account.service';
 import { SnackbarService } from '@_services/snackbar.service';
+import { Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 
 
 
@@ -20,10 +24,11 @@ import { SnackbarService } from '@_services/snackbar.service';
 })
 
 // Основной класс
-export class SettingsPersonProfileComponent {
+export class SettingsPersonProfileComponent implements OnDestroy {
 
 
   @ViewChild(ImageUploadComponent) appImageUpload: ImageUploadComponent;
+
   public form: FormGroup;
   public avatar: FormControl;
   public errors: ErrorMessagesType = ErrorMessages;
@@ -39,10 +44,13 @@ export class SettingsPersonProfileComponent {
   ];
   public fileLoaderKey: number = 0;
 
+  private destroy$: Subject<void> = new Subject<void>();
+
   constructor(
     private accountService: AccountService,
     private formBuilder: FormBuilder,
-    private snackbarService: SnackbarService
+    private snackbarService: SnackbarService,
+    private matDialog: MatDialog
   ) {
     // Настройка формы
     this.form = this.formBuilder.group({
@@ -61,24 +69,18 @@ export class SettingsPersonProfileComponent {
     });
     this.avatar = this.formBuilder.control(null);
     // Подписка на данные пользвателя
-    this.accountService.user$.subscribe(user => {
-      this.user = user;
-      // Значения формы
-      if (this.user) {
-        this.form.get("name").setValue(this.user.name);
-        this.form.get("lastName").setValue(this.user.lastName);
-        this.form.get("patronymic").setValue(this.user.patronymic);
-        this.form.get("birthDate").setValue(new Date(this.user.birthDate));
-        this.form.get("sex").setValue(this.user.sex == 1 ? true : false);
-        this.form.get("email").setValue(this.user.email);
-        this.avatar.setValue(this.user.avatars.full);
-      }
-    });
+    this.subscribeUser().subscribe();
   }
 
 
 
 
+
+  // Конец класса
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   // Сохранение данных
   public onSaveData(): void {
@@ -145,10 +147,12 @@ export class SettingsPersonProfileComponent {
           this.fileLoading = false;
           // Успешная загрузка аватарки
           if (code == "0001") {
-            this.accountService.syncCurrentUser().subscribe(() => this.snackbarService.open({
-              message: "Аватарка успешно загружена",
-              mode: "success"
-            }));
+            this.accountService.syncCurrentUser().subscribe(() => {
+              this.snackbarService.open({
+                message: "Аватарка успешно загружена",
+                mode: "success"
+              })
+            });
           }
         },
         () => {
@@ -166,6 +170,72 @@ export class SettingsPersonProfileComponent {
     }
   }
 
+  // Открыть окно обрезки
+  public onOpenCrop(type: UserAvatarCropDataKeys): void {
+    if (this.user) {
+      const data: PopupCropImageData = {
+        image: this.user.avatars.full,
+        coords: this.user.avatarCropData.crop,
+        minimal: [400, 400]
+      };
+      // Для обрезки основной фотки
+      if (type === "middle") {
+        data.image = this.user.avatars.crop;
+        data.coords = this.user.avatarCropData.middle;
+        data.aspectRatio = [1, 1];
+        data.minimal = [180, 180];
+      }
+      // Диалог
+      const dialog = PopupCropImageComponent.open(this.matDialog, data);
+      dialog.afterClosed().subscribe((position: UserAvatarCropDataElement | null) => this.onSaveCropPosition(type, position as UserAvatarCropDataElement));
+    }
+  }
+
+  // Сохранить позицию обрезанной фотки
+  public onSaveCropPosition(type: UserAvatarCropDataKeys, position: UserAvatarCropDataElement): void {
+    if (position) {
+      this.fileLoading = true;
+      // Запрос на сервер
+      this.accountService.cropAvatar(type, position).subscribe(
+        code => {
+          this.fileLoading = false;
+          // Успешная обрезка аватарки
+          if (code == "0001") {
+            this.accountService.syncCurrentUser().subscribe(() => {
+              this.snackbarService.open({
+                message: type === 'crop' ? "Основная аватарка успешно обрезана" : "Миниатюра аватарки успешно обрезана",
+                mode: "success"
+              })
+            });
+          }
+        },
+        () => this.fileLoading = false
+      );
+    }
+  }
+
+  // Удаление аватарки
+  public onDeleteAvatar(): void {
+    this.fileLoading = true;
+    // Запрос на сервер
+    this.accountService.deleteAvatar().subscribe(
+      code => {
+        this.fileLoading = false;
+        // Успешная обрезка аватарки
+        if (code == "0001") {
+          this.accountService.syncCurrentUser().subscribe(() => {
+            this.appImageUpload.clearInput(null);
+            this.snackbarService.open({
+              message: "Ваша аватарка успешно удалена",
+              mode: "success"
+            });
+          });
+        }
+      },
+      () => this.fileLoading = false
+    );
+  }
+
 
 
 
@@ -173,5 +243,31 @@ export class SettingsPersonProfileComponent {
   // Возраст до даты
   public ageToDate(age: number): Date {
     return new Date(Date.now() - (age * 365 * 24 * 60 * 60 * 1000));
+  }
+
+  // Подписка на пользователя
+  private subscribeUser(): Observable<User> {
+    return this.accountService.user$.pipe(
+      takeUntil(this.destroy$),
+      map(user => {
+        this.user = user;
+        // Значения формы
+        if (this.user) {
+          this.form.get("name").setValue(this.user.name);
+          this.form.get("lastName").setValue(this.user.lastName);
+          this.form.get("patronymic").setValue(this.user.patronymic);
+          this.form.get("birthDate").setValue(new Date(this.user.birthDate));
+          this.form.get("sex").setValue(this.user.sex == 1 ? true : false);
+          this.form.get("email").setValue(this.user.email);
+          this.avatar.setValue(this.user.avatars.full);
+        }
+        // Работа с аватаркой
+        if (this.appImageUpload) {
+          this.appImageUpload.clearInput(this.user.avatars.full);
+        }
+        // Вернуть юзера
+        return user;
+      })
+    );
   }
 }
