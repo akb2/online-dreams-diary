d128fc837bc415c4aa4a65eae73fc3b7d559913c
akb2 <akb2-online@ya.ru>
2021-08-19 01:59:27 +0300

    Добавил модель для экранов

---
+++ client/src/app/models/screen.ts
@@ -0,0 +1,21 @@
+// Ключи названий экранов
+export type ScreenKeys = "default" | "xsmall" | "small" | "middle" | "large" | "xlarge";
+
+// Тип данных для размеров экрана
+export interface ScreenBreakpoints {
+  default: number;
+  xsmall: number;
+  small: number;
+  middle: number;
+  large: number;
+  xlarge: number;
+}
+
+// Данные о загруженной картинке
+export class LoadingImageData {
+  constructor(
+    public url: string,
+    public width: number,
+    public height: number,
+  ) { }
+}
