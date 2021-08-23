8673e03bca2ef765d3a6778250844c00159e5ea7
akb2 <akb2-online@ya.ru>
2021-08-24 00:38:53 +0300

    Информация о токене

--- api/config/mysql_tables/token/checkToken.sql
+++ api/config/mysql_tables/token/checkToken.sql
@@ -3,7 +3,10 @@ SELECT
   `token`,
   `user_id`,
   `last_action_date`,
-  INET_NTOA(`ip`) AS `ip`
+  INET_NTOA(`ip`) AS `ip`,
+  `os`,
+  `browser`,
+  `browser_version`
 FROM `tokens`
 WHERE
   `token` = ?
