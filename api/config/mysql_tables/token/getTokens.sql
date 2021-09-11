14f4952d8c633f4e1ebc0828dfaa17318afee192
akb2 <akb2-online@ya.ru>
2021-09-11 16:18:26 +0300

    Запрос получения списка токенов

---
+++ api/config/mysql_tables/token/getTokens.sql
@@ -0,0 +1,14 @@
+SELECT
+  `id`,
+  `token`,
+  DATE_FORMAT(`create_date`, '%Y-%m-%dT%TZ') AS `create_date`,
+  DATE_FORMAT(`last_action_date`, '%Y-%m-%dT%TZ') AS `last_action_date`,
+  `user_id`,
+  INET_NTOA(`ip`) AS `ip`,
+  `os`,
+  `browser`,
+  `browser_version`
+FROM `tokens`
+WHERE
+  `user_id` = ? AND
+  `token` != ?
