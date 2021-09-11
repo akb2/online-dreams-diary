092ca88facdc832e86cf44fbc9bf9b0ace34506e
akb2 <akb2-online@ya.ru>
2021-09-11 16:20:17 +0300

    Вывел список токенов

--- client/src/app/pages/profile/settings/security/security.component.ts
+++ client/src/app/pages/profile/settings/security/security.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { User } from '@_models/account';
 import { BrowserNames, OsNames, SimpleObject } from '@_models/app';
 import { TokenInfo } from '@_models/token';
@@ -19,25 +19,39 @@ import { map, takeUntil } from 'rxjs/operators';
 })
 
 // Основной класс
-export class SettingsSecurityComponent {
+export class SettingsSecurityComponent implements OnInit, OnDestroy {
 
 
   public user: User;
   public tokenInfo: TokenInfo;
+  public tokensInfo: TokenInfo[];
   public osNames: SimpleObject = OsNames;
   public browserNames: SimpleObject = BrowserNames;
 
   public loadingTokenInfo: boolean = true;
+  public loadingTokensInfo: boolean = true;
 
   private destroy$: Subject<void> = new Subject<void>();
+
+
+
+
 
   constructor(
     private accountService: AccountService,
     private tokenService: TokenService
-  ) {
+  ) { }
+
+  ngOnInit() {
     this.getToken();
+    this.getTokens();
     // Подписка на данные пользвателя
     this.subscribeUser().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 
@@ -48,10 +62,27 @@ export class SettingsSecurityComponent {
   private getToken(): void {
     this.loadingTokenInfo = true;
     // Загрузка информации о токене
-    this.tokenService.getToken().subscribe(tokenInfo => {
-      this.loadingTokenInfo = false;
-      this.tokenInfo = tokenInfo;
-    });
+    this.tokenService.getToken().subscribe(
+      tokenInfo => {
+        this.loadingTokenInfo = false;
+        this.tokenInfo = tokenInfo;
+      },
+      () => this.loadingTokenInfo = false
+    );
+  }
+
+  // Получить сведения о всех токенах
+  private getTokens(): void {
+    this.loadingTokensInfo = true;
+    // Загрузка информации о токене
+    this.tokenService.getTokens(true).subscribe(
+      tokensInfo => {
+        this.loadingTokensInfo = false;
+        this.tokensInfo = tokensInfo;
+      },
+      error =>
+        console.log(error)
+    );
   }
 
   // Подписка на пользователя
