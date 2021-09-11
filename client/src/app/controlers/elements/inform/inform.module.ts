6f08d98750535fc1e2b3ea0088754016c20bf519
akb2 <akb2-online@ya.ru>
2021-09-11 16:18:59 +0300

    Форматирование

--- client/src/app/controlers/elements/inform/inform.module.ts
+++ client/src/app/controlers/elements/inform/inform.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
 import { InformComponent } from "./inform.component";
+import { MatIconModule } from "@angular/material/icon";
 
 
 
@@ -18,9 +18,5 @@ import { InformComponent } from "./inform.component";
     CommonModule
   ]
 })
-
-
-
-
 
 export class InformModule { }
