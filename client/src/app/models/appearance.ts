644b0b2c07318f3bff299437a00e8987d7ac27e7
akb2 <akb2-online@ya.ru>
2021-09-19 02:52:42 +0300

    Модель фонов шапки

---
+++ client/src/app/models/appearance.ts
@@ -0,0 +1,54 @@
+// Интерфейс картинки фона
+export interface BackgroundImageData {
+  id: number;
+  title: string;
+  imageName: string;
+  positionX: "left" | "center" | "right";
+  positionY: "top" | "center" | "bottom";
+  imageOverlay: boolean;
+}
+
+// Массив картинок фона
+export const BackgroundImageDatas: BackgroundImageData[] = [{
+  id: 2,
+  title: "Лестница в небо",
+  imageName: "2.jpg",
+  positionX: "center",
+  positionY: "center",
+  imageOverlay: true
+}, {
+  id: 3,
+  title: "Космическая станция на орбите",
+  imageName: "3.jpg",
+  positionX: "center",
+  positionY: "center",
+  imageOverlay: false
+}, {
+  id: 4,
+  title: "Странник на краю скалы у моря",
+  imageName: "4.jpg",
+  positionX: "center",
+  positionY: "center",
+  imageOverlay: true
+}, {
+  id: 5,
+  title: "Волк в тумане хвойного леса",
+  imageName: "5.jpg",
+  positionX: "center",
+  positionY: "bottom",
+  imageOverlay: true
+}, {
+  id: 6,
+  title: "Карманные часы на песке",
+  imageName: "6.jpg",
+  positionX: "right",
+  positionY: "center",
+  imageOverlay: true
+}, {
+  id: 7,
+  title: "Зимняя ночь большого города",
+  imageName: "7.jpg",
+  positionX: "center",
+  positionY: "top",
+  imageOverlay: true
+}];
