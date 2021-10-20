1494f9b1faef69fe2d9578509cc0ae915af1f045
akb2 <akb2-online@ya.ru>
2021-10-21 01:42:44 +0300

    Сервис для сновидений

---
+++ client/src/app/services/dream.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from "@angular/core";
+import { User } from "@_models/account";
+import { BackgroundImageDatas } from "@_models/appearance";
+import { Dream, DreamMode } from "@_models/dream";
+import { NavMenuType } from "@_models/nav-menu";
+import { AccountService } from "@_services/account.service";
+
+
+
+
+
+@Injectable({
+  providedIn: "root"
+})
+
+export class DreamService {
+
+
+  private currentUser: User;
+
+
+
+
+
+  constructor(
+    private accountService: AccountService
+  ) {
+    this.currentUser = this.accountService.getCurrentUser();
+    // Подписка на актуальные сведения о пользователе
+    this.accountService.user$.subscribe(user => user ? this.currentUser = user : null);
+  }
+
+
+
+
+
+  newDream(): Dream {
+    return {
+      id: 0,
+      user: this.currentUser,
+      createDate: null,
+      date: null,
+      title: "",
+      keywords: [],
+      text: "",
+      places: [],
+      members: [],
+      map: null,
+      mode: DreamMode.text,
+      headerType: NavMenuType.short,
+      headerBackground: BackgroundImageDatas.find(b => b.id === 11)
+    };
+  }
+}
