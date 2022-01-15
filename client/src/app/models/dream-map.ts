8a063a0bc968d22d7b64ce351c2b83af4ed8d448
akb2 <akb2-online@ya.ru>
2022-01-15 18:49:59 +0300

    Разделил модели сновидений и карт

---
+++ client/src/app/models/dream-map.ts
@@ -0,0 +1,145 @@
+import { Place } from "@_models/dream";
+import { Light } from "three";
+
+
+
+
+
+// Интерфейс карты
+export interface DreamMap {
+  ceils: (DreamMapCeil | null)[];
+  size: MapSize;
+  dreamerWay: DreamerWay[] | null;
+  skyBox: number;
+}
+
+// Интерфейс ячейки сновидения
+export interface DreamMapCeil {
+  place: Place | null;
+  terrain: number;
+  object: MapObject | null;
+  coord: Coord
+}
+
+// Интерфейс типа местности
+export interface MapTerrain {
+  id: number;
+  name: string;
+  title: string;
+}
+
+// Интерфейс типа неба
+export interface MapSkyBox {
+  id: number;
+  name: string;
+  title: string;
+  fogColor: number;
+  lights: MapSkyBoxLight[];
+}
+
+// Интерфейс освещения
+interface MapSkyBoxLight {
+  light: Light;
+  target: SkyBoxLightTarget;
+  position?: Coord;
+  fixed?: boolean;
+  shadow?: {
+    near: number;
+    far: number;
+    top: number;
+    left: number;
+    right: number;
+    bottom: number;
+    width: number;
+    height: number;
+    radius: number;
+  };
+}
+
+// Перебор для назначения освещения
+export enum SkyBoxLightTarget {
+  Scene = "Scene",
+  Camera = "Camera"
+}
+
+// Интерфейс объекта карты
+export interface MapObject {
+  id: number;
+  name: string;
+}
+
+// Интерфейс карты для сервера
+export interface DreamMapDto {
+  ceils: (DreamMapCeilDto | null)[];
+  size: MapSize;
+  dreamerWay: DreamerWay[] | null;
+  skyBox: number | null;
+}
+
+// Интерфейс ячейки сновидения
+export interface DreamMapCeilDto {
+  place: number | null;
+  terrain: number | null;
+  object: number | null;
+  coord: Coord
+}
+
+
+
+
+
+// Интерфейс пути сновидца в сновидении
+export interface DreamerWay {
+  coord: Coord;
+  color: WayColor;
+  lineType: WayLineType;
+}
+
+// Интерфейс размера
+export interface MapSize {
+  width: number;
+  height: number;
+}
+
+// Интерфейс координат
+export interface Coord {
+  x: number;
+  y: number;
+  z: number;
+}
+
+// Типы цветов пути
+export type WayColor = "red" | "green" | "blue" | "white" | "black" | "gray" | "orange" | "pink" | "pink" | "purple";
+
+// Тип линии
+export interface WayLineType {
+  type: CssBorderType;
+  title: string;
+  description: string;
+}
+
+// Тип CSS границы
+export type CssBorderType = "solid" | "double" | "dashed" | "dotted";
+
+
+
+
+
+// Набор типов линий
+export const WayLineTypes: WayLineType[] = [{
+  type: "solid",
+  title: "Ходьба / езда",
+  description: "Перемещение пешком, автомобиле, велосипеде и т.д."
+}, {
+  type: "double",
+  title: "Полет",
+  description: "Персонаж летит сам, или на транспорте, или другом существе"
+}, {
+  type: "dashed",
+  title: "Плавание",
+  description: "Персонаж плывет сам, или на транспорте, или на другом существе"
+}, {
+  type: "dotted",
+  title: "Телепортация",
+  description: "Персонаж перемещается из одной точки в другую, минуя пространство"
+}];
--- client/src/app/models/dream.ts
+++ client/src/app/models/dream.ts
@@ -1,8 +1,8 @@
 import { OptionData } from "@_controlers/autocomplete-input/autocomplete-input.component";
 import { User } from "@_models/account";
 import { BackgroundImageData } from "@_models/appearance";
+import { DreamMap } from "@_models/dream-map";
 import { NavMenuType } from "@_models/nav-menu";
-import { Light } from "three";
 
 
 
@@ -55,112 +55,10 @@ export interface Place {
   isReal: boolean;
 }
 
-// Интерфейс пути сновидца в сновидении
-export interface DreamerWay {
-  coord: Coord;
-  color: WayColor;
-  lineType: WayLineType;
-}
 
 
 
 
-
-// Интерфейс карты
-export interface DreamMap {
-  ceils: (DreamMapCeil | null)[];
-  size: MapSize;
-  dreamerWay: DreamerWay[] | null;
-  skyBox: number;
-}
-
-// Интерфейс ячейки сновидения
-export interface DreamMapCeil {
-  place: Place | null;
-  terrain: number;
-  object: MapObject | null;
-  coord: Coord
-}
-
-// Интерфейс типа местности
-export interface MapTerrain {
-  id: number;
-  name: string;
-  title: string;
-}
-
-// Интерфейс типа неба
-export interface MapSkyBox {
-  id: number;
-  name: string;
-  title: string;
-  fogColor: number;
-  lights: MapSkyBoxLight[];
-}
-
-// Интерфейс освещения
-interface MapSkyBoxLight {
-  light: Light;
-  target: SkyBoxLightTarget;
-  position?: Coord;
-  fixed?: boolean;
-  shadow?: {
-    near: number;
-    far: number;
-    top: number;
-    left: number;
-    right: number;
-    bottom: number;
-    width: number;
-    height: number;
-    radius: number;
-  };
-}
-
-// Перебор для назначения освещения
-export enum SkyBoxLightTarget {
-  Scene = "Scene",
-  Camera = "Camera"
-}
-
-// Интерфейс объекта карты
-export interface MapObject {
-  id: number;
-  name: string;
-}
-
-// Интерфейс карты для сервера
-export interface DreamMapDto {
-  ceils: (DreamMapCeilDto | null)[];
-  size: MapSize;
-  dreamerWay: DreamerWay[] | null;
-  skyBox: number | null;
-}
-
-// Интерфейс ячейки сновидения
-export interface DreamMapCeilDto {
-  place: number | null;
-  terrain: number | null;
-  object: number | null;
-  coord: Coord
-}
-
-
-
-
-
-// Интерфейс размера
-export interface MapSize {
-  width: number;
-  height: number;
-}
-
-// Интерфейс координат
-export interface Coord {
-  x: number;
-  y: number;
-  z: number;
-}
 
 // Тип сновидения
 export enum DreamMode {
@@ -179,43 +77,11 @@ export enum DreamStatus {
   public, // Доступен всем в интернете
 }
 
-// Типы цветов пути
-export type WayColor = "red" | "green" | "blue" | "white" | "black" | "gray" | "orange" | "pink" | "pink" | "purple";
-
-// Тип линии
-export interface WayLineType {
-  type: CssBorderType;
-  title: string;
-  description: string;
-}
-
-// Тип CSS границы
-export type CssBorderType = "solid" | "double" | "dashed" | "dotted";
 
 
 
 
-
-// Набор типов линий
-export const WayLineTypes: WayLineType[] = [{
-  type: "solid",
-  title: "Ходьба / езда",
-  description: "Перемещение пешком, автомобиле, велосипеде и т.д."
-}, {
-  type: "double",
-  title: "Полет",
-  description: "Персонаж летит сам, или на транспорте, или другом существе"
-}, {
-  type: "dashed",
-  title: "Плавание",
-  description: "Персонаж плывет сам, или на транспорте, или на другом существе"
-}, {
-  type: "dotted",
-  title: "Телепортация",
-  description: "Персонаж перемещается из одной точки в другую, минуя пространство"
-}];
-
-// Набор методов для типа новидения
+// Набор методов для типа сновидения
 export const DreamModes: OptionData[] = [{
   key: DreamMode.text.toString(),
   title: "В виде текста",
