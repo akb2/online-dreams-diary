11bc5c1a8f3279042d5f28c8392303a0f1a337fe
akb2 <akb2-online@ya.ru>
2022-02-11 04:45:09 +0300

    Получение сновидения по ID и создание нового сновидения

--- api/services/dream.php
+++ api/services/dream.php
@@ -2,6 +2,7 @@
 
 namespace OnlineDreamsDiary\Services;
 
+use OnlineDreamsDiary\Models\Dream;
 use PDO;
 
 
@@ -45,5 +46,52 @@ class DreamService
     }
     // Результат работы функции
     return false;
+  }
+
+
+
+  // Получить сновидение по ID
+  public function getById(string $id, string $userId): array
+  {
+    if ($id > 0) {
+      $sqlData = array($id);
+      $dream = $this->dataBaseService->getDatasFromFile("dream/getById.sql", $sqlData);
+      // Проверить сновидение
+      if (count($dream) > 0) {
+        if ($dream[0]["user_id"] == $userId || !($userId > 0)) {
+          return $dream[0];
+        }
+      }
+    }
+    // Сон не найден
+    return array();
+  }
+
+
+
+  // Новое сновидение
+  public function createDream($data): int
+  {
+    $sqlData = array(
+      "user_id" => strval($data["userId"]),
+      "mode" => strval($data["mode"]),
+      "status" => strval($data["status"]),
+      "date" => date("Y-m-d", strtotime($data["date"])),
+      "title" => $data["title"],
+      "description" => $data["description"],
+      "keywords" => $data["keywords"],
+      "text" => $data["text"],
+      "places" => $data["places"],
+      "members" => $data["members"],
+      "map" => $data["map"],
+      "header_type" => $data["headerType"],
+      "header_background" => strval($data["headerBackgroundId"]),
+    );
+    // Попытка сохранения
+    if ($this->dataBaseService->executeFromFile("dream/create.sql", $sqlData)) {
+      return $this->pdo->lastInsertId();
+    }
+    // Сновидение не сохранено
+    return 0;
   }
 }
