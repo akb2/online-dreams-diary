53694a6712a104eec07ed7b7da572a5b2a16148a
akb2 <akb2-online@ya.ru>
2022-02-11 17:03:13 +0300

    Получение сновидения по ID и добавил проверку по токену

--- api/controllers/dream.php
+++ api/controllers/dream.php
@@ -4,9 +4,11 @@ namespace OnlineDreamsDiary\Controllers;
 
 include_once "services/database.php";
 include_once "services/dream.php";
+include_once "services/token.php";
 include_once "config/database.php";
 
 use OnlineDreamsDiary\Services\DreamService;
+use OnlineDreamsDiary\Services\TokenService;
 use PDO;
 
 
@@ -18,6 +20,7 @@ class Dream
   private PDO $pdo;
 
   private DreamService $dreamService;
+  private TokenService $tokenService;
 
 
 
@@ -37,6 +40,7 @@ class Dream
   public function setServices(): void
   {
     $this->dreamService = new DreamService($this->pdo, $this->config);
+    $this->tokenService = new TokenService($this->pdo, $this->config);
   }
 
 
@@ -47,19 +51,85 @@ class Dream
   {
     $id = 0;
     $code = "7001";
-    $dream = $this->dreamService->getById($data["id"], $data["userId"]);
-    // Переписать старое
-    if (isset($dream["id"]) && $dream["id"] > 0) {
+    $userId = $_GET["user_id"];
+    $token = $_GET["token"];
+    $dream = $this->dreamService->getById($data["id"], $userId);
+    // Проверка доступности
+    if ($this->tokenService->checkToken($userId, $token)) {
+      // Переписать старое
+      if (isset($dream["id"]) && $dream["id"] > 0) {
+        $id = $this->dreamService->updateDream($data);
+      }
+      // Новое сновидение
+      else {
+        $id = $this->dreamService->createDream($data);
+      }
     }
-    // Новое сновидение
+    // Неверный токен
     else {
-      $id = $this->dreamService->createDream($data);
-      $code = $id > 0 ? "0001" : $code;
+      $code = "9015";
     }
+    // Обновить код
+    $code = $id > 0 ? "0001" : $code;
     // Сновидение не сохранено
     return array(
       "code" => $code,
-      "id" => $id
+      "data" => $id
+    );
+  }
+
+  // Получить сновидение
+  // * GET
+  public function getById($data): array
+  {
+    $code = "0002";
+    $userId = $_GET["user_id"];
+    $token = $_GET["token"];
+    $testDream = $this->dreamService->getById($data["id"]);
+    $dream = array();
+    // Проверка доступности
+    if ($this->tokenService->checkToken($userId, $token)) {
+      // Сновидение найдено
+      if (isset($testDream["id"]) && $testDream["id"] > 0) {
+        // Доступность для просмотра
+        if ($this->dreamService->checkAvail($testDream["id"], $userId)) {
+          $code = "0001";
+          $dream = array(
+            "id" => intval($testDream["id"]),
+            "userId" => intval($testDream["user_id"]),
+            "createDate" => $testDream["create_date"],
+            "date" => $testDream["date"],
+            "title" => $testDream["title"],
+            "description" => $testDream["description"],
+            "keywords" => $testDream["keywords"],
+            "text" => $testDream["text"],
+            "places" => $testDream["places"],
+            "members" => $testDream["members"],
+            "map" => $testDream["map"],
+            "mode" => intval($testDream["mode"]),
+            "status" => intval($testDream["status"]),
+            "headerType" => $testDream["header_type"],
+            "headerBackgroundId" => intval($testDream["header_background"])
+          );
+        }
+        // Нельяз смотреть
+        else {
+          $code = "7002";
+        }
+      }
+      // Сновидение не найдено
+      else {
+        $code = "0002";
+      }
+    }
+    // Неверный токен
+    else {
+      $code = "9015";
+    }
+    // Вернуть результат
+    return array(
+      "data" => $dream,
+      "code" => $code
     );
   }
 }
