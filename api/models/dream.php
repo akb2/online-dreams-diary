63893529997ee9139b02c166bb09846d20dc5148
akb2 <akb2-online@ya.ru>
2022-02-11 04:45:20 +0300

    Модель сновидения

---
+++ api/models/dream.php
@@ -0,0 +1,25 @@
+<?
+
+namespace OnlineDreamsDiary\Models;
+
+
+
+// Основной класс сновидения
+interface Dream
+{
+  public int $id;
+  public int $userId;
+  public string $createDate;
+  public string $date;
+  public string $title;
+  public string $description;
+  public string $keywords;
+  public string $text;
+  public string $places;
+  public string $members;
+  public string $map;
+  public int $mode;
+  public int $status;
+  public string $headerType;
+  public int $headerBackgroundId;
+}
