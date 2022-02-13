7a350fe82ba00ae2ad73d383f53e7fd554e109a1
akb2 <akb2-online@ya.ru>
2022-02-13 23:53:15 +0300

    Добавил меню действий

--- client/src/app/controlers/lists/dream-list/dream-list.component.ts
+++ client/src/app/controlers/lists/dream-list/dream-list.component.ts
@@ -1,6 +1,11 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DoCheck, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { AppComponent } from "@app/app.component";
+import { CardMenuItem } from "@_controlers/card-menu/card-menu.component";
+import { User } from "@_models/account";
+import { CustomObjectKey } from "@_models/app";
 import { Dream } from "@_models/dream";
 import { NavMenuType } from "@_models/nav-menu";
+import { AccountService } from "@_services/account.service";
 import { DreamDescription, DreamTitle } from "@_services/dream.service";
 
 
@@ -14,7 +19,7 @@ import { DreamDescription, DreamTitle } from "@_services/dream.service";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class DreamListComponent {
+export class DreamListComponent implements DoCheck, OnChanges {
 
 
   @Input() dreams: Dream[];
@@ -24,12 +29,100 @@ export class DreamListComponent {
   today: Date = new Date();
   imagePrefix: string = "../../../../assets/images/backgrounds/";
 
+  oldUser: User;
+  dreamsMenuItems: CustomObjectKey<number, CardMenuItem[]> = {};
 
 
 
 
-  // Без картинки
+
+  // Текущий пользователь
+  get user(): User {
+    return AppComponent.user;
+  };
+
+  // Есть обложка
   isHasImage(dream: Dream): boolean {
     return dream.headerType === NavMenuType.full || dream.headerType === NavMenuType.short;
+  }
+
+
+
+
+
+  constructor(
+    private accountService: AccountService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) { }
+
+  ngDoCheck() {
+    if (this.accountService.checkAuth && this.oldUser?.id !== this.user?.id) {
+      this.oldUser = this.user;
+      this.changeDetectorRef.detectChanges();
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.dreams) {
+      this.createMenu();
+    }
+  }
+
+
+
+
+
+  // Удаление сновидения
+  onDreamDelete(dream: Dream): void {
+    console.log(dream);
+  }
+
+
+
+
+
+  // Ссылки на сновидение
+  private createMenu(): void {
+    this.dreamsMenuItems = {};
+    // Заполнить список меню
+    this.dreams.forEach(dream => {
+      const menuItem: CardMenuItem[] = [];
+      const divider: CardMenuItem = { delimeter: true };
+      // Просмотр сновидения
+      menuItem.push({
+        icon: "visibility",
+        title: "Просмотр",
+        routerLink: "/diary/viewer/" + dream.id
+      });
+      // Разделитель
+      menuItem.push(divider);
+      // Для собственных сновидений
+      if (dream.user.id === this.user?.id) {
+        // Редактирование
+        menuItem.push({
+          icon: "edit",
+          title: "Редактировать",
+          routerLink: "/diary/editor/" + dream.id
+        });
+        // Удаление
+        menuItem.push({
+          icon: "edit",
+          title: "Удалить",
+          callback: this.onDreamDelete.bind(this, dream)
+        });
+      }
+      // Для сновидений других пользователей
+      else {
+        // Профиль
+        menuItem.push({
+          icon: "account_circle",
+          title: "Профиль автора",
+          subTitle: dream.user.name + " " + dream.user.lastName,
+          routerLink: "/profile/" + dream.user.id
+        });
+      }
+      // Вернуть список пунктов
+      this.dreamsMenuItems[dream.id] = menuItem;
+    });
   }
 }
--- client/src/app/controlers/lists/dream-list/dream-list.module.ts
+++ client/src/app/controlers/lists/dream-list/dream-list.module.ts
@@ -1,7 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { RouterModule } from "@angular/router";
 import { DreamListComponent } from "@_controlers/dream-list/dream-list.component";
 import { CoreModule } from "@_modules/core.module";
+import { MatDividerModule } from "@angular/material/divider";
+import { CardMenuModule } from "@_controlers/card-menu/card-menu.module";
 
 
 
@@ -16,7 +22,13 @@ import { CoreModule } from "@_modules/core.module";
   ],
   imports: [
     CommonModule,
-    CoreModule
+    CoreModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    RouterModule,
+    MatDividerModule,
+    CardMenuModule
   ]
 })
 
