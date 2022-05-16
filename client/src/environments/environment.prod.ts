04a67e9faac370f8606d2fdcf46efa8719011a69
akb2 <akb2-online@ya.ru>
2022-05-16 18:16:00 +0300

    Убрал кавычки

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  baseUrl: "http://api.dreams-diary.ru",
+  baseUrl: "http://api.dreams-diary.ru/",
   httpHeader: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "multipart/form-data",
-    "Authorization": "token"
+    Authorization: "token"
   }
 };
