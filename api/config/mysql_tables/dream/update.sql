17f50832166d2c73c541fab71bcddfcf271cde4f
akb2 <akb2-online@ya.ru>
2022-08-06 16:08:57 +0400

    Обновление даты изменения

--- api/config/mysql_tables/dream/update.sql
+++ api/config/mysql_tables/dream/update.sql
@@ -11,6 +11,7 @@ SET
   `members` = :members,
   `map` = :map,
   `header_type` = :header_type,
-  `header_background` = :header_background
+  `header_background` = :header_background,
+  `edit_date` = CURRENT_TIMESTAMP
 WHERE
   `id` = :id
