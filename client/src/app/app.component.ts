4ce440d413a26e49d7d25a4e4e378fbaebd8db79
akb2 <akb2-online@ya.ru>
2022-08-08 03:12:50 +0400

    Функция заголовка; оптимизация кода

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 import { ActivatedRoute, ActivatedRouteSnapshot, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from "@angular/router";
 import { User } from "@_models/account";
-import { RouteData } from "@_models/app";
+import { CustomObject, RouteData } from "@_models/app";
 import { AccountService } from "@_services/account.service";
 import { ApiService } from "@_services/api.service";
 import { SnackbarService } from "@_services/snackbar.service";
@@ -23,16 +23,30 @@ import { delay, switchMap, takeUntil, takeWhile } from "rxjs/operators";
 export class AppComponent implements OnInit, OnDestroy {
 
 
-  public static user: User;
+  static user: User;
 
-  public mainTitle: string = "Online Dreams Diary";
+  private static mainTitle: string = "Online Dreams Diary";
+  private static titleSeparator: string = "|";
 
-  public showPreloader: boolean = true;
-  public validToken: boolean = false;
+  validToken: boolean = false;
+  showPreLoader: boolean = true;
   private pageData: RouteData;
   private loaderDelay: number = 150;
 
   private destroy$: Subject<void> = new Subject();
+
+
+
+
+
+  // Дополнительные данные страницы
+  private get getExtraDatas(): ExtraDatas {
+    const extraDatas: CustomObject<any> = this.router.getCurrentNavigation()?.extras.state ?? {};
+    // Найти данные
+    return Object.entries(DefaultExtraDatas)
+      .map(([k, v]) => ([k, extraDatas.hasOwnProperty(k) ? !!extraDatas[k] : v]))
+      .reduce((o, [k, v]) => ({ ...o, [k as string]: !!v }), {} as ExtraDatas);
+  }
 
 
 
@@ -48,27 +62,6 @@ export class AppComponent implements OnInit, OnDestroy {
     private activatedRoute: ActivatedRoute
   ) {
     this.validToken = false;
-    // Подписка на пользователя
-    this.accountService.user$.subscribe(
-      user => AppComponent.user = user,
-      () => AppComponent.user = null
-    );
-    // События старта и окочания лоадера
-    this.router.events.pipe(takeUntil(this.destroy$)).subscribe(event => {
-      if (event instanceof NavigationStart) {
-        this.beforeLoadPage();
-      }
-
-      else if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
-        let snapshots: ActivatedRouteSnapshot = this.activatedRoute.snapshot;
-        while (snapshots.firstChild) {
-          snapshots = snapshots.firstChild;
-        }
-        this.pageData = snapshots.data;
-        // Функция обработчик
-        this.afterLoadPage();
-      }
-    });
   }
 
   ngOnInit() {
@@ -76,6 +69,25 @@ export class AppComponent implements OnInit, OnDestroy {
       user => AppComponent.user = user,
       () => AppComponent.user = null
     );
+    // Подписка на пользователя
+    this.accountService.user$.subscribe(
+      user => AppComponent.user = user,
+      () => AppComponent.user = null
+    );
+    // События старта и окочания лоадера
+    this.router.events
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(event => {
+        if (event instanceof NavigationStart) {
+          this.beforeLoadPage();
+        }
+        // Конец роута
+        else if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+          this.pageData = AppComponent.getPageData(this.activatedRoute.snapshot);
+          // Функция обработчик
+          this.afterLoadPage();
+        }
+      });
   }
 
   ngOnDestroy() {
@@ -89,8 +101,8 @@ export class AppComponent implements OnInit, OnDestroy {
 
   // Действия перед загрузкой страницы
   private beforeLoadPage(): void {
-    // Пользователь авторизован, проверить токен
-    const checkToken: boolean = !(this.router.getCurrentNavigation()?.extras.state?.checkToken == false);
+    const { checkToken, showPreLoader }: ExtraDatas = this.getExtraDatas;
+    // ПРоверить токен
     if (this.accountService.checkAuth && checkToken) {
       this.tokenService.checkToken(["9014", "9015", "9016"]).subscribe(code => {
         // Если токен валидный
@@ -116,26 +128,27 @@ export class AppComponent implements OnInit, OnDestroy {
       this.validToken = true;
     }
     // Запуск прелоадера
-    const showPreloader: boolean = !(this.router.getCurrentNavigation()?.extras.state?.showPreLoader == false);
-    if (showPreloader) {
-      this.showPreloader = true;
+    if (showPreLoader) {
+      this.showPreLoader = true;
       document.querySelectorAll("body, html").forEach(elm => elm.classList.add("no-scroll"));
     }
   }
 
   // Действия после загрузки страницы
   private afterLoadPage(): void {
-    // Название страницы
-    const title: string = this.pageData?.title || "";
-    this.titleService.setTitle((title ? title + " | " : "") + this.mainTitle);
+    const { changeTitle }: ExtraDatas = this.getExtraDatas;
+    // Сменить название страницы
+    if (changeTitle) {
+      this.titleService.setTitle(AppComponent.createTitle(this.pageData?.title));
+    }
     // Отключение прелоадера
     timer(0, 100).pipe(
-      takeWhile(() => this.showPreloader, true),
+      takeWhile(() => this.showPreLoader, true),
       switchMap(() => of(this.validToken)),
       delay(this.loaderDelay)
     ).subscribe(t => {
       if (t) {
-        this.showPreloader = false;
+        this.showPreLoader = false;
         document.querySelectorAll("body, html").forEach(elm => elm.classList.remove("no-scroll"));
       }
     });
@@ -145,4 +158,55 @@ export class AppComponent implements OnInit, OnDestroy {
   private subscribeUser(): Observable<User> {
     return this.accountService.user$.pipe(takeUntil(this.destroy$));
   }
+
+
+
+
+
+  // Создать текст заголовка
+  static createTitle(mixedTitle: string | string[], subTitle: string = null, separator: string = null): string {
+    subTitle = subTitle ?? AppComponent.mainTitle;
+    separator = separator ?? AppComponent.titleSeparator;
+    // Объеденить заголовки
+    const title: string = Array.isArray(mixedTitle) ?
+      mixedTitle.filter(t => !!t).join(" " + separator + " ") :
+      mixedTitle;
+    // Заголовок + подзаголовок
+    if (!!title && !!subTitle) {
+      return title + " " + separator + " " + subTitle;
+    }
+    // Только заголовок или подзаголовок
+    else if (!!title || !!subTitle) {
+      return !!title ? title : subTitle;
+    }
+    // Подзаголовок
+    else {
+      return AppComponent.mainTitle;
+    }
+  }
+
+  // Данные страинцы
+  static getPageData(snapshots: ActivatedRouteSnapshot): RouteData {
+    while (!!snapshots.firstChild) {
+      snapshots = snapshots.firstChild;
+    }
+    // Вернуть данные
+    return snapshots.data;
+  }
 }
+
+
+
+
+
+interface ExtraDatas {
+  checkToken: boolean;
+  showPreLoader: boolean;
+  changeTitle: boolean;
+}
+
+const DefaultExtraDatas: ExtraDatas = {
+  checkToken: true,
+  showPreLoader: true,
+  changeTitle: true
+};
