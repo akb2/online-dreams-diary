6ac90b0bac417e572b49667ab4f40b42e0070bc7
akb2 <akb2-online@ya.ru>
2022-08-08 04:06:47 +0400

    Подключил модули

--- client/src/app/pages/people/people.module.ts
+++ client/src/app/pages/people/people.module.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { InformModule } from '@_controlers/inform/inform.module';
 import { NavMenuModule } from "@_controlers/nav-menu/nav-menu.module";
+import { PaginationModule } from '@_controlers/pagination/pagination.module';
+import { PeopleRoutingModule } from '@_pages/people/people-routing.module';
 import { PeopleComponent } from '@_pages/people/people.component';
 
 
@@ -14,6 +17,9 @@ import { PeopleComponent } from '@_pages/people/people.component';
   imports: [
     CommonModule,
     NavMenuModule,
+    PeopleRoutingModule,
+    InformModule,
+    PaginationModule
   ]
 })
 
