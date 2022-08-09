ff700b7bc1282b84b901def8be0ec5e0adf2104e
akb2 <akb2-online@ya.ru>
2022-08-10 02:14:28 +0400

    Подключил кнопки и базовые модули

--- client/src/app/pages/people/people.module.ts
+++ client/src/app/pages/people/people.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { InformModule } from '@_controlers/inform/inform.module';
 import { NavMenuModule } from "@_controlers/nav-menu/nav-menu.module";
 import { PaginationModule } from '@_controlers/pagination/pagination.module';
+import { CoreModule } from '@_modules/core.module';
 import { PeopleRoutingModule } from '@_pages/people/people-routing.module';
 import { PeopleComponent } from '@_pages/people/people.component';
 
@@ -15,11 +17,13 @@ import { PeopleComponent } from '@_pages/people/people.component';
     PeopleComponent,
   ],
   imports: [
+    CoreModule,
     CommonModule,
     NavMenuModule,
     PeopleRoutingModule,
     InformModule,
-    PaginationModule
+    PaginationModule,
+    MatButtonModule
   ]
 })
 
