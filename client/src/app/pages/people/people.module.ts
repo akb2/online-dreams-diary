0c1af4b85d58359f5c53966e3d1947af69ac7ece
akb2 <akb2-online@ya.ru>
2022-08-12 22:26:09 +0400

    Подключил компонент панели поиска

--- client/src/app/pages/people/people.module.ts
+++ client/src/app/pages/people/people.module.ts
@@ -5,6 +5,7 @@ import { InformModule } from '@_controlers/inform/inform.module';
 import { NavMenuModule } from "@_controlers/nav-menu/nav-menu.module";
 import { PaginationModule } from '@_controlers/pagination/pagination.module';
 import { PeopleListModule } from '@_controlers/people-list/people-list.module';
+import { SearchPanelModule } from '@_controlers/search-panel/search-panel.module';
 import { CoreModule } from '@_modules/core.module';
 import { PeopleRoutingModule } from '@_pages/people/people-routing.module';
 import { PeopleComponent } from '@_pages/people/people.component';
@@ -25,7 +26,8 @@ import { PeopleComponent } from '@_pages/people/people.component';
     InformModule,
     PaginationModule,
     MatButtonModule,
-    PeopleListModule
+    PeopleListModule,
+    SearchPanelModule
   ]
 })
 
