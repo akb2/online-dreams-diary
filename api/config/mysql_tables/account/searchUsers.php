a73f30e822ca3f8a00626addcbb16450e31d51b3
akb2 <akb2-online@ya.ru>
2022-08-13 22:52:24 +0400

    Добавил фильтр по ФИО

--- api/config/mysql_tables/account/searchUsers.php
+++ api/config/mysql_tables/account/searchUsers.php
@@ -13,8 +13,19 @@ SELECT
   `email`,
   `roles`,
   `avatar_crop_data`
+
+  <?/* Поиск по поисковому запросу + сортировка */?>
+  <? if(strlen($input["q"]) > 0): ?>
+    , MATCH (`name`, `last_name`, `patronymic`) AGAINST (:q IN BOOLEAN MODE) AS `q`
+  <? endif;?>
 FROM `users`
 WHERE
   `id` > 0
-ORDER BY `id` ASC
+
+  <?/* Поиск по поисковому запросу */?>
+  <? if(strlen($input["q"]) > 0): ?>
+    AND MATCH (`name`, `last_name`, `patronymic`) AGAINST (:q IN BOOLEAN MODE)
+  <? endif;?>
+
+ORDER BY  <? if(strlen($input["q"]) > 0): ?>`q` DESC,<? endif;?> `id` ASC
 LIMIT <?=$input['limit_start'];?>, <?=$input['limit_length'];?>
--- api/config/mysql_tables/account/searchUsersCount.php
+++ api/config/mysql_tables/account/searchUsersCount.php
@@ -2,3 +2,8 @@ SELECT COUNT(*)
 FROM `users`
 WHERE
   `id` > 0
+
+  <?/* Поиск по поисковому запросу */?>
+  <? if(strlen($input["q"]) > 0): ?>
+    AND MATCH (`name`, `last_name`, `patronymic`) AGAINST (:q IN BOOLEAN MODE)
+  <? endif;?>
