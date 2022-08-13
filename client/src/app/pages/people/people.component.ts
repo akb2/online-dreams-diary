8287ca7b74602dd3dc9028c37405f0f5781d5052
akb2 <akb2-online@ya.ru>
2022-08-13 20:51:18 +0400

    Добавил поиск по имени

--- client/src/app/pages/people/people.component.ts
+++ client/src/app/pages/people/people.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PaginateEvent } from '@_controlers/pagination/pagination.component';
 import { SearchPanelComponent } from '@_controlers/search-panel/search-panel.component';
 import { User } from '@_models/account';
-import { SimpleObject } from '@_models/app';
+import { CustomObject, SimpleObject } from '@_models/app';
 import { BackgroundImageData, BackgroundImageDatas } from '@_models/appearance';
 import { NavMenuType } from '@_models/nav-menu';
 import { ScreenService } from '@_services/screen.service';
@@ -42,6 +43,8 @@ export class PeopleComponent implements OnInit, OnDestroy {
   pageLimit: number = 1;
   pageCount: number = 1;
 
+  searchForm: FormGroup;
+
   peoplePlural: SimpleObject = {
     "=0": "",
     "=1": "# человек",
@@ -62,6 +65,34 @@ export class PeopleComponent implements OnInit, OnDestroy {
     return this.pageCount > this.pageLimit / 2;
   }
 
+  // Данные поиска
+  private get getSearch(): SearchUser {
+    const page: number = this.pageCurrent > 0 ? this.pageCurrent : 1;
+    // Вернуть данные
+    return {
+      page,
+      q: this.searchForm.get("q")?.value as string ?? ""
+    };
+  }
+
+  // Текущие данные из URL
+  private get getCurrentSearch(): SearchUser {
+    const page: number = parseInt(this.queryParams.page) > 0 ? parseInt(this.queryParams.page) : 1;
+    // Вернуть данные
+    return {
+      page,
+      q: this.queryParams.q as string ?? ""
+    };
+  }
+
+  // Пустые данные
+  private get getDefaultSearch(): SearchUser {
+    return {
+      q: "",
+      page: 1
+    };
+  }
+
 
 
 
@@ -71,13 +102,24 @@ export class PeopleComponent implements OnInit, OnDestroy {
     private router: Router,
     private changeDetectorRef: ChangeDetectorRef,
     private userService: UserService,
-    private screenService: ScreenService
-  ) { }
+    private screenService: ScreenService,
+    private formBuilder: FormBuilder
+  ) {
+    // Создать форму
+    this.searchForm = this.formBuilder.group({
+      q: [""]
+    });
+  }
 
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe(params => {
       this.queryParams = params as SimpleObject;
-      this.pageCurrent = parseInt(params.p) || 1;
+      // Параметры из URL
+      this.pageCurrent = parseInt(params.page) || 1;
+      // Наполнить форму
+      Object.entries(this.getCurrentSearch)
+        .filter(([k]) => k !== "page")
+        .forEach(([k, v]) => this.searchForm.get(k)?.setValue(v));
       // Поиск пользователей
       this.search();
     });
@@ -110,21 +152,19 @@ export class PeopleComponent implements OnInit, OnDestroy {
 
   // Изменение страницы
   onPageChange(event: PaginateEvent): void {
-    const path: string[] = (this.router.url.split("?")[0]).split("/").filter(v => v.length > 0);
-    // Настройки
     this.pageCurrent = event.pageCurrent;
-    // Перейти к новой странице
-    this.router.navigate(path, {
-      queryParams: { ...this.queryParams, p: event.pageCurrent },
-      queryParamsHandling: "merge",
-      replaceUrl: true,
-      state: {
-        changeTitle: false,
-        showPreLoader: false
-      }
-    });
-    // Обновить список
-    this.search();
+    // Изменить URL
+    this.urlSet(this.getSearch);
+  }
+
+  // Событие поиска
+  onSearch(): void {
+    this.urlSet(this.getSearch);
+  }
+
+  // Сбросить поиск
+  onClear(): void {
+    this.urlSet(this.getDefaultSearch);
   }
 
 
@@ -132,15 +172,11 @@ export class PeopleComponent implements OnInit, OnDestroy {
 
 
   // Загрузка списка сновидений
-  search(): void {
+  private search(): void {
     this.loading = true;
     this.changeDetectorRef.detectChanges();
-    // Поиск по сновидениям
-    const search: SearchUser = {
-      page: this.pageCurrent > 0 ? this.pageCurrent : 1
-    };
     // Загрузка списка
-    this.userService.search(search, ["0002"]).subscribe(
+    this.userService.search(this.getSearch, ["0002"]).subscribe(
       ({ count, result: people, limit }) => {
         // Найдены сновидения
         if (count > 0) {
@@ -164,5 +200,24 @@ export class PeopleComponent implements OnInit, OnDestroy {
   // Показать фильтры
   openSearch(): void {
     this.searchPanel?.openPanel();
+  }
+
+  // Записать параметры в URL
+  private urlSet(datas: SearchUser | CustomObject<string | number>): void {
+    const path: string[] = (this.router.url.split("?")[0]).split("/").filter(v => v.length > 0);
+    const queryParams: CustomObject<string | number | null> = Object.entries({ ...this.queryParams, ...datas })
+      .map(([k, v]) => ([k, !!v ? v : null]))
+      .reduce((o, [k, v]) => ({ ...o, [k]: v }), {});
+    console.log(queryParams);
+    // Перейти к новой странице
+    this.router.navigate(path, {
+      queryParams,
+      queryParamsHandling: "merge",
+      replaceUrl: true,
+      state: {
+        changeTitle: false,
+        showPreLoader: false
+      }
+    });
   }
 }
--- client/src/app/pages/people/people.module.ts
+++ client/src/app/pages/people/people.module.ts
@@ -5,7 +5,9 @@ import { InformModule } from '@_controlers/inform/inform.module';
 import { NavMenuModule } from "@_controlers/nav-menu/nav-menu.module";
 import { PaginationModule } from '@_controlers/pagination/pagination.module';
 import { PeopleListModule } from '@_controlers/people-list/people-list.module';
+import { SearchGroupModule } from '@_controlers/search-group/search-group.module';
 import { SearchPanelModule } from '@_controlers/search-panel/search-panel.module';
+import { TextInputModule } from '@_controlers/text-input/text-input.module';
 import { CoreModule } from '@_modules/core.module';
 import { PeopleRoutingModule } from '@_pages/people/people-routing.module';
 import { PeopleComponent } from '@_pages/people/people.component';
@@ -27,7 +29,9 @@ import { PeopleComponent } from '@_pages/people/people.component';
     PaginationModule,
     MatButtonModule,
     PeopleListModule,
-    SearchPanelModule
+    SearchPanelModule,
+    SearchGroupModule,
+    TextInputModule
   ]
 })
 
