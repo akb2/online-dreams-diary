f6df479471144f5e758e5dc051c5bfa026d4f0be
akb2 <akb2-online@ya.ru>
2022-08-15 02:56:13 +0400

    Поиск по дате рождения

--- client/src/app/pages/people/people.component.ts
+++ client/src/app/pages/people/people.component.ts
@@ -1,15 +1,18 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DefaultExtraDatas, ExtraDatas as ExtraDatasApp } from '@app/app.component';
+import { OptionData } from '@_controlers/autocomplete-input/autocomplete-input.component';
 import { PaginateEvent } from '@_controlers/pagination/pagination.component';
 import { SearchPanelComponent } from '@_controlers/search-panel/search-panel.component';
 import { User } from '@_models/account';
-import { CustomObject, SimpleObject } from '@_models/app';
+import { CustomObject, CustomObjectKey, SimpleObject } from '@_models/app';
 import { BackgroundImageData, BackgroundImageDatas } from '@_models/appearance';
+import { FormData } from '@_models/form';
 import { NavMenuType } from '@_models/nav-menu';
 import { ScreenService } from '@_services/screen.service';
 import { SearchUser, UserService } from '@_services/user.service';
-import { Subject, takeUntil } from 'rxjs';
+import { merge, Subject, takeUntil } from 'rxjs';
 
 
 
@@ -44,6 +47,9 @@ export class PeopleComponent implements OnInit, OnDestroy {
   pageCount: number = 1;
 
   searchForm: FormGroup;
+  birthYears: OptionData[] = [];
+  birthMonths: OptionData[] = [];
+  birthDays: OptionData[] = [];
 
   peoplePlural: SimpleObject = {
     "=0": "",
@@ -51,6 +57,21 @@ export class PeopleComponent implements OnInit, OnDestroy {
     "few": "# человека",
     "other": "# человек"
   };
+
+  private monthPlural: string[] = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь",
+  ];
 
   private queryParams: SimpleObject = {};
 
@@ -68,20 +89,26 @@ export class PeopleComponent implements OnInit, OnDestroy {
   // Данные поиска
   private get getSearch(): SearchUser {
     const page: number = this.pageCurrent > 0 ? this.pageCurrent : 1;
+    const fromForm: CustomObjectKey<keyof SearchUser, string | number> = Object.entries(this.getDefaultSearch)
+      .filter(([k]) => k !== "page")
+      .reduce((o, [k]) => ({ ...o, [k]: this.searchForm.get(k)?.value?.toString() ?? "" }), {});
     // Вернуть данные
     return {
-      page,
-      q: this.searchForm.get("q")?.value as string ?? ""
+      ...fromForm,
+      page
     };
   }
 
   // Текущие данные из URL
   private get getCurrentSearch(): SearchUser {
     const page: number = parseInt(this.queryParams.page) > 0 ? parseInt(this.queryParams.page) : 1;
+    const fromForm: CustomObjectKey<keyof SearchUser, string | number> = Object.entries(this.getDefaultSearch)
+      .filter(([k]) => k !== "page")
+      .reduce((o, [k]) => ({ ...o, [k]: this.queryParams[k] as string ?? "" }), {});
     // Вернуть данные
     return {
+      ...fromForm,
       page,
-      q: this.queryParams.q as string ?? ""
     };
   }
 
@@ -89,8 +116,20 @@ export class PeopleComponent implements OnInit, OnDestroy {
   private get getDefaultSearch(): SearchUser {
     return {
       q: "",
+      birthDay: "",
+      birthMonth: "",
+      birthYear: "",
       page: 1
     };
+  }
+
+  // Дополнительные данные страницы
+  private get getExtraDatas(): ExtraDatas {
+    const extraDatas: CustomObject<any> = this.router.getCurrentNavigation()?.extras.state ?? {};
+    // Найти данные
+    return Object.entries({ DefaultExtraDatas, noUpdateSearch: false })
+      .map(([k, v]) => ([k, extraDatas.hasOwnProperty(k) ? !!extraDatas[k] : v]))
+      .reduce((o, [k, v]) => ({ ...o, [k as string]: !!v }), {} as ExtraDatas);
   }
 
 
@@ -105,23 +144,33 @@ export class PeopleComponent implements OnInit, OnDestroy {
     private screenService: ScreenService,
     private formBuilder: FormBuilder
   ) {
-    // Создать форму
-    this.searchForm = this.formBuilder.group({
-      q: [""]
-    });
+    this.searchForm = this.formBuilder.group(Object.entries(this.getDefaultSearch)
+      .filter(([k]) => k !== "page")
+      .reduce((o, [k, v]) => ({ ...o, [k]: [v, null] }), {})
+    );
+    // Заполнить списки выбора
+    this.fillYearsOptionData();
+    this.fillMonthsOptionData();
+    // Изменение списка дней
+    merge(this.searchForm.get("birthMonth").valueChanges, this.searchForm.get("birthYear").valueChanges)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => this.fillDaysOptionData());
   }
 
   ngOnInit() {
     this.activatedRoute.queryParams.subscribe(params => {
+      const { noUpdateSearch }: ExtraDatas = this.getExtraDatas;
+      // Данные из URL
       this.queryParams = params as SimpleObject;
-      // Параметры из URL
       this.pageCurrent = parseInt(params.page) || 1;
       // Наполнить форму
       Object.entries(this.getCurrentSearch)
         .filter(([k]) => k !== "page")
         .forEach(([k, v]) => this.searchForm.get(k)?.setValue(v));
       // Поиск пользователей
-      this.search();
+      if (!noUpdateSearch) {
+        this.search();
+      }
     });
     // Подписка на тип устройства
     this.screenService.isMobile$
@@ -171,6 +220,69 @@ export class PeopleComponent implements OnInit, OnDestroy {
 
 
 
+  // Заполнить список годов
+  private fillYearsOptionData(): void {
+    const currYear: number = (new Date()).getFullYear();
+    const minYear: number = FormData.birthDateProjectYear - FormData.birthDateMaxAge;
+    const maxYear: number = currYear - FormData.birthDateMinAge;
+    const yearsPeriod: number = maxYear - minYear + 1;
+    // Заполнить данные
+    this.birthYears = (new Array(yearsPeriod)).fill(1).map((k, i) => (maxYear - i).toString()).map(year => ({
+      key: year.toString(),
+      iconColor: "primary",
+      title: year.toString()
+    }));
+    // Добавить пункт любой год
+    this.birthYears.unshift({
+      key: "",
+      title: "Любой год"
+    });
+  }
+
+  // Заполнить список месяцев
+  private fillMonthsOptionData(): void {
+    // Заполнить данные
+    this.birthMonths = (new Array(this.monthPlural.length)).fill(1).map((k, i) => ({
+      key: (i + 1).toString(),
+      iconColor: "primary",
+      title: this.monthPlural[i].toString()
+    }));
+    // Добавить пункт любой месяц
+    this.birthMonths.unshift({
+      key: "",
+      title: "Любой месяц"
+    });
+  }
+
+  // Заполнить список месяцев
+  private fillDaysOptionData(): void {
+    const year: number = parseInt(this.getSearch.birthYear) || 0;
+    const month: number = (parseInt(this.getSearch.birthMonth) || 0);
+    const days: number = year === 0 && month === 2 ? 29 : new Date(year, month, 0).getDate();
+    const day: number = parseInt(this.getSearch.birthDay) || 0;
+    const newDay: string = day > days ? "" : (day > 0 ? day : "").toString();
+    // Заполнить данные
+    this.birthDays = (new Array(days)).fill(1).map((k, i) => (i + 1).toString()).map(key => ({
+      key,
+      iconColor: "primary",
+      title: key
+    }));
+    // Добавить пункт любой месяц
+    this.birthDays.unshift({
+      key: "",
+      title: "Любой день"
+    });
+    // Изменить значение, если оно не вписывается в массив
+    this.searchForm.get("birthDay").setValue(newDay);
+    this.urlSet(this.getSearch, true);
+    // Обновить
+    this.changeDetectorRef.detectChanges();
+  }
+
+
+
+
+
   // Загрузка списка сновидений
   private search(): void {
     this.loading = true;
@@ -203,12 +315,11 @@ export class PeopleComponent implements OnInit, OnDestroy {
   }
 
   // Записать параметры в URL
-  private urlSet(datas: SearchUser | CustomObject<string | number>): void {
+  private urlSet(datas: SearchUser | CustomObject<string | number>, noUpdateSearch: boolean = false): void {
     const path: string[] = (this.router.url.split("?")[0]).split("/").filter(v => v.length > 0);
     const queryParams: CustomObject<string | number | null> = Object.entries({ ...this.queryParams, ...datas })
       .map(([k, v]) => ([k, !!v ? v : null]))
       .reduce((o, [k, v]) => ({ ...o, [k]: v }), {});
-    console.log(queryParams);
     // Перейти к новой странице
     this.router.navigate(path, {
       queryParams,
@@ -216,8 +327,18 @@ export class PeopleComponent implements OnInit, OnDestroy {
       replaceUrl: true,
       state: {
         changeTitle: false,
-        showPreLoader: false
+        showPreLoader: false,
+        noUpdateSearch
       }
     });
   }
+}
+
+
+
+
+
+// Интерфейс дополнительных данных страницы
+interface ExtraDatas extends ExtraDatasApp {
+  noUpdateSearch: boolean;
 }
