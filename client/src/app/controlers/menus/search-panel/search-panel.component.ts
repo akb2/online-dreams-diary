fb3d8d8c2a96d0e6dc29e78bdde75b7e3fe2b484
akb2 <akb2-online@ya.ru>
2022-08-15 02:57:07 +0400

    Скрыл панель при запуске

--- client/src/app/controlers/menus/search-panel/search-panel.component.ts
+++ client/src/app/controlers/menus/search-panel/search-panel.component.ts
@@ -24,7 +24,7 @@ export class SearchPanelComponent {
   @Output() submit: EventEmitter<void> = new EventEmitter<void>();
   @Output() clear: EventEmitter<void> = new EventEmitter<void>();
 
-  isShow: boolean = true;
+  isShow: boolean = false;
 
 
 
