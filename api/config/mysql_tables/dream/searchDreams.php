7421d5c0f48ea6769a020a7e9c3705814df67bf4
akb2 <akb2-online@ya.ru>
2022-08-22 02:36:44 +0400

    Разбил условия по статусам

--- api/config/mysql_tables/dream/searchDreams.php
+++ api/config/mysql_tables/dream/searchDreams.php
@@ -25,19 +25,24 @@ WHERE
 
   <?/* Фильтр по статусам: определенного пользователя */?>
   <?/* // ? !friends(3)!, *users(4)*, public(5) */?>
-  <? elseif($input['user_id'] > 0 && $input["user_id"] !== $input["current_user"]): ?>
+  <? elseif ($input['user_id'] > 0 && $input['check_token'] && $input["user_id"] !== $input["current_user"]): ?>
     AND (
       `status` = 5
       <? if($input['check_token']): ?> OR `status` = 4 <? endif; ?>
     )
 
-  <?/* Фильтр по статусам: Общий дневник */?>
+  <?/* Фильтр по статусам: Общий дневник: Авторизованный пользователь */?>
   <?/* // ? *users(4)*, public(5) */?>
-  <? else: ?>
+  <? elseif ($input['check_token']): ?>
     AND (
       `status` = 5
       <? if($input['check_token']): ?> OR `status` = 4 <? endif; ?>
     )
+
+  <?/* Фильтр по статусам: Общий дневник: Неавторизованный пользователь */?>
+  <?/* // ? public(5) */?>
+  <? else: ?>
+    AND `status` = 5
   <? endif; ?>
 
 ORDER BY `date` DESC
