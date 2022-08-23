953a1e1679f7f8517455cda971208a14c5cdef0a
akb2 <akb2-online@ya.ru>
2022-08-24 02:52:34 +0400

    Убрал карту с главной

--- client/src/app/pages/home/home.module.ts
+++ client/src/app/pages/home/home.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { DreamMapEditorModule } from '@_controlers/dream-map-editor/dream-map-editor.module';
 import { NavMenuModule } from "@_controlers/nav-menu/nav-menu.module";
 import { CoreModule } from '@_modules/core.module';
 import { HomeRoutingModule } from './home-routing.module';
@@ -16,8 +15,7 @@ import { HomeComponent } from './home.component';
   imports: [
     CoreModule,
     HomeRoutingModule,
-    NavMenuModule,
-    DreamMapEditorModule
+    NavMenuModule
   ]
 })
 
