e2ccacfc939dffc339abf4714ff78442557ca5b2
akb2 <akb2-online@ya.ru>
2022-12-26 09:23:32 +0400

    Замена переменных в письме

--- api/services/Mail.php
+++ api/services/Mail.php
@@ -8,19 +8,16 @@ class MailService
 {
   private array $config;
 
-  private string $senderMail;
-  private string $templateDir;
   private array $headers;
 
   public function __construct(array $config)
   {
     $this->config = $config;
-    // Определить переменные
-    $this->senderMail = $this->config['mail']['sender'] ? $this->config['mail']['sender'] : $this->senderMail;
-    $this->templateDir = $this->config['mail']['templateDir'] ? $this->config['mail']['templateDir'] : $this->templateDir;
     // Конфигурирование
     $this->headers = array(
-      'From' => $this->senderMail,
+      'From' => "Online Dreams Diary <" . $this->config['mail']['sender'] . ">",
+      'Return-Path' => $this->config['mail']['sender'],
+      'Content-type' => 'text/html; charset=utf-8',
       'X-Mailer' => 'PHP/' . phpversion()
     );
   }
@@ -31,35 +28,50 @@ class MailService
   public function send(string $template, string $receiver, string $title, array $params = array()): bool
   {
     if (strlen($receiver) > 0 && strlen($title) > 0) {
-      if (preg_match('/$[0-9a-z\-_\.]+@[0-9a-z\-_\.]+^/i', $receiver)) {
-        $templatePath = $this->templateDir . $template . '.html';
-        // Проверить шаблон
-        if (file_exists($templatePath)) {
-          $content = file_get_contents($templatePath);
-          preg_match('/\{\{([A-z0-9\-_]{1,32})\}\}/gm', $content, $keys);
-          // Поиск и замена переменных в шаблоне
-          // if (count($keys) > 0) {
-          //   foreach ($keys as  $keys) {
-          //     $key = $keys[1];
-          //     // Поиск вхождения
-          //     if (preg_match('/\{\{' . $key . '\}\}/gm', $content) && isset($params[$key])) {
-          //       $content = preg_replace('/\{\{' . $key . '\}\}/gm', $params[$key], $content);
-          //     }
-          //   }
-          // }
-          // Отправка письма
-          if (strlen($content) > 0) {
-            return mail(
-              $receiver,
-              $title,
-              $content,
-              $this->headers
-            );
-          }
+      if (preg_match('/^[0-9a-z\-_\.]+@[0-9a-z\-_\.]+$/i', $receiver)) {
+        $content = $this->getTemplate($template, $params);
+        // Отправка письма
+        if (strlen($content) > 0) {
+          return mail(
+            $receiver,
+            $title,
+            $content,
+            $this->headers
+          );
         }
       }
     }
     // Не удалось отправить письмо
     return false;
+  }
+
+
+
+  // Заменить в шаблоне переменные
+  private function getTemplate(string $template, array $params): string
+  {
+    $templatePath = $this->config['mail']['templateDir'] . $template . '.html';
+    // Todo: удалить сохранение обработанного письма
+    $templateTempPath = $this->config['mail']['templateDir'] . $template . '.test.html';
+    // Проверка пути шаблона
+    if (file_exists($templatePath)) {
+      $content = file_get_contents($templatePath);
+      // Поиск переменных в шаблоне
+      preg_match_all('/\{\{([A-z0-9\-_]{1,32})\}\}/m', $content, $keys);
+      // Замена переменных в шаблоне
+      if (isset($keys[1]) && count($keys[1]) > 0) {
+        foreach ($keys[1] as $key) {
+          if (preg_match('/\{\{' . $key . '\}\}/m', $content) && isset($params[$key])) {
+            $content = preg_replace('/\{\{' . $key . '\}\}/m', $params[$key], $content);
+          }
+        }
+      }
+      // Todo: удалить сохранение обработанного письма
+      file_put_contents($templateTempPath, $content);
+      // Вернуть текст шаблона
+      return $content;
+    }
+    // Пустое письмо
+    return "";
   }
 }
