ff93e940b7e4448c20e71d7eda9823d2539a1ac3
akb2 <akb2-online@ya.ru>
2022-12-29 02:30:14 +0400

    Сравнение массивов и объектов

---
+++ client/src/app/helpers/objects.ts
@@ -0,0 +1,44 @@
+// Сравнение двух объектов
+export const CompareObjects: <T>(objA: T, objB: T) => boolean = <T>(objA: T, objB: T) => {
+  const simpleType: boolean = !(!!objA && !!objB && ((Array.isArray(objA) && Array.isArray(objB)) || (typeof objA === "object" && typeof objB === "object")));
+  // Простой тип
+  if (simpleType) {
+    return objA === objB;
+  }
+  // Массив
+  else if (Array.isArray(objA) && Array.isArray(objB)) {
+    return CompareArrays(objA, objB);
+  }
+  // Объекты
+  else if (typeof objA === "object" && typeof objB === "object") {
+    const arrayA: [string, any][] = Object.entries(objA);
+    const arrayB: [string, any][] = Object.entries(objB);
+    // Вернуть результат проверки
+    return arrayA.length === arrayB.length && arrayA.every((v, k) => {
+      const vA: any = v;
+      const vB: any = arrayB[k];
+      const simpleType: boolean = !(!!vA && !!vB && ((Array.isArray(vA) && Array.isArray(vB)) || (typeof vA === "object" && typeof vB === "object")));
+      // Вернуть результат проверки
+      return (
+        (Array.isArray(vA) && Array.isArray(vB) && CompareArrays(vA, vB)) ||
+        (typeof vA === "object" && typeof vB === "object" && CompareObjects(vA, vB)) ||
+        (simpleType && vA === vB)
+      );
+    });
+  }
+  // Разные типы данных
+  return false;
+};
+
+// Сравнение двух масивов
+export const CompareArrays: <T>(arrayA: T[], arrayB: T[]) => boolean = <T>(arrayA: T[], arrayB: T[]) => arrayA?.length === arrayB?.length && arrayA.every((v, k) => {
+  const vA: T = v;
+  const vB: T = arrayB[k];
+  const simpleType: boolean = !(!!vA && !!vB && ((Array.isArray(vA) && Array.isArray(vB)) || (typeof vA === "object" && typeof vB === "object")));
+  // Вернуть результат проверки
+  return (
+    (Array.isArray(vA) && Array.isArray(vB) && CompareArrays(vA, vB)) ||
+    (typeof vA === "object" && typeof vB === "object" && CompareObjects(vA, vB)) ||
+    (simpleType && vA === vB)
+  );
+});
