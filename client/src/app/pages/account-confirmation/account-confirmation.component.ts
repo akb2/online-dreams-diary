a6750cbf6688cc8883af61f29721ca652ea69ca6
akb2 <akb2-online@ya.ru>
2022-12-30 02:14:50 +0400

    Запрос активации аккаунта

--- client/src/app/pages/account-confirmation/account-confirmation.component.ts
+++ client/src/app/pages/account-confirmation/account-confirmation.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { NavMenuType } from "@_models/nav-menu";
+import { AccountService } from "@_services/account.service";
+import { Subject, takeUntil } from "rxjs";
 
 
 
@@ -13,23 +15,75 @@ import { NavMenuType } from "@_models/nav-menu";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class AccountConfirmationComponent {
+export class AccountConfirmationComponent implements OnInit, OnDestroy {
 
 
   navMenuType: NavMenuType = NavMenuType.collapse;
 
   activationLoader: boolean = false;
   activationSuccess: boolean = false;
+  alreadyActivate: boolean = false;
 
+  private activationUser: number;
   private activationCode: string;
+
+  private destroy$: Subject<void> = new Subject();
 
 
 
 
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private accountService: AccountService,
+    private changeDetectorRef: ChangeDetectorRef
   ) {
+    this.activationUser = parseInt(this.activatedRoute?.snapshot?.params?.userId) ?? 0;
     this.activationCode = this.activatedRoute?.snapshot?.params?.activationCode?.toString() ?? "";
+  }
+
+  ngOnInit(): void {
+    this.activationLoader = true;
+    this.alreadyActivate = false;
+    // Попытка активации
+    if (this.activationUser > 0 && !!this.activationCode?.length) {
+      this.accountService.activateAccount(this.activationUser, this.activationCode, ["9026"])
+        .pipe(takeUntil(this.destroy$))
+        .subscribe(
+          code => {
+            if (code === "0001") {
+              this.activationLoader = false;
+              this.activationSuccess = true;
+              this.changeDetectorRef.detectChanges();
+            }
+            // Аккаунт уже активирован
+            else if (code === "9026") {
+              this.alreadyActivate = true;
+              this.onActivationError();
+            }
+          },
+          () => this.onActivationError()
+        );
+    }
+    // Не получены данные
+    else {
+      this.onActivationError();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+
+
+
+
+  // Ошибка активации аккаунта
+  private onActivationError(): void {
+    this.activationLoader = false;
+    this.activationSuccess = false;
+    this.changeDetectorRef.detectChanges();
   }
 }
