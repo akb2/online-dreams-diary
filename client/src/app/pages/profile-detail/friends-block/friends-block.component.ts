79b2cb6600d9988a640c34ee78f86b78d21f075e
akb2 <akb2-online@ya.ru>
2023-01-11 23:58:44 +0400

    Создал компонент списка друзей

---
+++ client/src/app/pages/profile-detail/friends-block/friends-block.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+
+
+
+
+@Component({
+  selector: "friends-block",
+  templateUrl: "friends-block.component.html",
+  styleUrls: ["friends-block.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class FriendsBlockComponent {
+}
--- client/src/app/pages/profile-detail/profile-detail.module.ts
+++ client/src/app/pages/profile-detail/profile-detail.module.ts
@@ -4,20 +4,21 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { CardModule } from '@_controlers/card/card.module';
 import { DreamListModule } from '@_controlers/dream-list/dream-list.module';
 import { InformModule } from '@_controlers/inform/inform.module';
 import { NavMenuModule } from "@_controlers/nav-menu/nav-menu.module";
 import { PageLoaderModule } from '@_controlers/page-loader/page-loader.module';
 import { TitleModule } from '@_controlers/title/title.module';
 import { CoreModule } from '@_modules/core.module';
-import { GeneralInfoBlockComponent } from './general-info-block/general-info-block.component';
+import { DreamsBlockComponent } from '@_pages/profile-detail/dreams-block/dreams-block.component';
+import { FriendsBlockComponent } from '@_pages/profile-detail/friends-block/friends-block.component';
 import { ActionBlockComponent } from './action-block/action-block.component';
 import { AvatarBlockComponent } from './avatar-block/avatar-block.component';
+import { GeneralInfoBlockComponent } from './general-info-block/general-info-block.component';
 import { ProfileDetailRoutingModule } from './profile-detail-routing.module';
 import { ProfileDetailComponent } from './profile-detail.component';
 import { StatusBlockComponent } from './status-block/status-block.component';
-import { CardModule } from '@_controlers/card/card.module';
-import { DreamsBlockComponent } from '@_pages/profile-detail/dreams-block/dreams-block.component';
 
 
 
@@ -30,7 +31,8 @@ import { DreamsBlockComponent } from '@_pages/profile-detail/dreams-block/dreams
     StatusBlockComponent,
     ActionBlockComponent,
     GeneralInfoBlockComponent,
-    DreamsBlockComponent
+    DreamsBlockComponent,
+    FriendsBlockComponent
   ],
   imports: [
     CommonModule,
