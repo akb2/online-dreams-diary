4d6eb459c535b622bb381324849bd8b7bd89e7a2
akb2 <akb2-online@ya.ru>
2023-01-11 04:59:49 +0400

    Запрос удаления из друзей

---
+++ api/config/mysql_tables/friend/cancelFromFriendsByUsers.sql
@@ -0,0 +1,13 @@
+UPDATE `friends`
+SET
+  `status` = IF(`in_user_id` = :in_user_id, 0, 2),
+  `in_date` = CURRENT_TIMESTAMP
+WHERE (
+  (
+    `out_user_id` = :out_user_id AND
+    `in_user_id` = :in_user_id
+  ) OR (
+    `in_user_id` = :out_user_id AND
+    `out_user_id` = :in_user_id
+  )
+)
