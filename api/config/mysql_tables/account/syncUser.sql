72ca69ce848beabbe664a5422cc4cdd221ca9a0b
akb2 <akb2-online@ya.ru>
2023-01-16 00:17:48 +0400

    Запрос для синхронизации данных

---
+++ api/config/mysql_tables/account/syncUser.sql
@@ -0,0 +1,24 @@
+SELECT
+  `id`,
+  `page_status`,
+  `activation_key`,
+  `activation_key_expire`,
+  `settings`,
+  `private`,
+  `status`,
+  `login`,
+  `name`,
+  `last_name`,
+  `patronymic`,
+  DATE_FORMAT(`register_date`, '%Y-%m-%dT%TZ') AS `register_date`,
+  DATE_FORMAT(`last_action_date`, '%Y-%m-%dT%TZ') AS `last_action_date`,
+  DATE_FORMAT(`last_edit_date`, '%Y-%m-%dT%TZ') AS `last_edit_date`,
+  `birth_date`,
+  `sex`,
+  `email`,
+  `roles`,
+  `avatar_crop_data`
+FROM `users`
+WHERE
+  `id` = ? AND
+  `last_edit_date` > ?
