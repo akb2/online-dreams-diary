599d055378e41124725332367280c7bf004c481e
akb2 <akb2-online@ya.ru>
2023-03-05 01:51:48 +0400

    Список типов и настроений сновидений

--- client/src/app/datas/dream.ts
+++ client/src/app/datas/dream.ts
@@ -1,10 +1,16 @@
 import { OptionData } from "@_controlers/autocomplete-input/autocomplete-input.component";
 import { SimpleObject } from "@_models/app";
-import { DreamMode, DreamStatus } from "@_models/dream";
+import { DreamMode, DreamMood, DreamStatus, DreamType } from "@_models/dream";
 
 
 
 
+
+// Префикс для картинок типа сновидений
+const DreamTypeImagePreffix: string = "/assets/images/icons/dream-type/";
+
+// Префикс для картинок настроения сновидений
+const DreamMoodImagePreffix: string = "/assets/images/icons/dream-mood/";
 
 // Набор методов для типа сновидения
 export const DreamModes: OptionData[] = [
@@ -91,6 +97,103 @@ export const DreamStatuses: OptionData[] = [
     iconBackground: "fill"
   }
 ];
+
+// Информация о типах сновидений
+export const DreamTypes: OptionData[] = [
+  // Обычное сновидение
+  {
+    key: DreamType.Simple.toString(),
+    title: "Обычное сновидение",
+    subTitle: "В этом сновидении не произошло ничего, что можно было отнести к другим категориям.",
+    image: "simple"
+  },
+  // Сплошная болтовня
+  {
+    key: DreamType.Chatter.toString(),
+    title: "Сплошная болтовня",
+    subTitle: "В этом типе мало активных действий, мало сведений о ландшафте, но много диалогов.",
+    image: "chatter"
+  },
+  // Полнейший бред
+  {
+    key: DreamType.Drivel.toString(),
+    title: "Полнейший бред",
+    subTitle: "Данным типом помечайте даже те сновидения, которые являются бредом для сновидения.",
+    image: "drivel"
+  },
+  // Эпичное
+  {
+    key: DreamType.Epic.toString(),
+    title: "Эпичное",
+    subTitle: "В данный тип входят сновидения о космических битвах или других грандиозных событиях снов.",
+    image: "epic"
+  },
+  // Осознанное сновидение
+  {
+    key: DreamType.Lucid.toString(),
+    title: "Осознанное сновидение",
+    subTitle: "В данный тип входят ОС'ы или так называемые \"ВТО\".",
+    image: "lucid"
+  },
+]
+  .map(option => ({
+    ...option,
+    image: DreamTypeImagePreffix + option.image + ".png",
+    imagePosition: "contain",
+    iconBackground: "transparent"
+  }));
+
+// Информация о настроении сновидений
+export const DreamMoods: OptionData[] = [
+  // Ночной кошмар
+  {
+    key: DreamMood.Nightmare.toString(),
+    title: "Ночной кошмар",
+    subTitle: "В это настроение входят: погони от монстров, сонные параличи и т.д.",
+    image: "nightmare"
+  },
+  // Мрачный сон
+  {
+    key: DreamMood.Gloomy.toString(),
+    title: "Мрачный сон",
+    subTitle: "Отличается тем, что явной угрозы нет, но чувствуется присутствие чего-то угрожающего. Обычно картинка темная/черно-белая или не включается свет.",
+    image: "gloomy"
+  },
+  // Грустный сон
+  {
+    key: DreamMood.Sad.toString(),
+    title: "Грустный сон",
+    subTitle: "Тут может обычная повседневность, но события показывают потерю близких или другие сильные негативные эмоции.",
+    image: "sad"
+  },
+  // Обычный сон
+  {
+    key: DreamMood.Nothing.toString(),
+    title: "Обычный сон",
+    subTitle: "Сюжет сна не вызвал в вас сильной эмоцианальной отдачи.",
+    image: "nothing"
+  },
+  // Веселый сон
+  {
+    key: DreamMood.Joy.toString(),
+    title: "Веселый сон",
+    subTitle: "Такие сюжеты могут вызвать либо смех, либо эмоцианальной подъем и вдохновение.",
+    image: "joy"
+  },
+  // Филосовский сон
+  {
+    key: DreamMood.Philosophy.toString(),
+    title: "Филосовский сон",
+    subTitle: "После таких сновидений мы можете годами обдумывать смысл жизни/мироустройство/ваше место в мире.",
+    image: "philosophy"
+  },
+]
+  .map(option => ({
+    ...option,
+    image: DreamMoodImagePreffix + option.image + ".png",
+    imagePosition: "contain",
+    iconBackground: "transparent"
+  }))
 
 // Склонение количества сновидений
 export const DreamPlural: SimpleObject = {
