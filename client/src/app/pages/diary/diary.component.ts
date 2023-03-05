25df95c4d8bdf14540846d247e92edb9da8af6ae
akb2 <akb2-online@ya.ru>
2023-03-05 21:18:03 +0400

    Поиск по запросу

--- client/src/app/pages/diary/diary.component.ts
+++ client/src/app/pages/diary/diary.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 import { Title } from "@angular/platform-browser";
 import { ActivatedRoute, Router } from "@angular/router";
 import { PaginateEvent } from "@_controlers/pagination/pagination.component";
@@ -6,8 +7,9 @@ import { SearchPanelComponent } from "@_controlers/search-panel/search-panel.com
 import { BackgroundImageDatas } from "@_datas/appearance";
 import { DreamPlural } from "@_datas/dream";
 import { CheckInRange, ParseInt } from "@_helpers/math";
+import { CompareObjects } from "@_helpers/objects";
 import { User } from "@_models/account";
-import { RouteData, SimpleObject } from "@_models/app";
+import { CustomObject, CustomObjectKey, RouteData, SimpleObject } from "@_models/app";
 import { BackgroundImageData } from "@_models/appearance";
 import { Dream } from "@_models/dream";
 import { NavMenuType } from "@_models/nav-menu";
@@ -61,7 +63,8 @@ export class DiaryComponent implements OnInit, OnDestroy {
   dreamsCount: number = 0;
 
   pageCurrent: number = 1;
-  pageLimit: number = 1;
+  pageLimit: number = 12;
+  private defaultPageLimit: number = 12;
   private pageCount: number = 1;
 
   isMobile: boolean = false;
@@ -70,6 +73,8 @@ export class DiaryComponent implements OnInit, OnDestroy {
   navMenuType: typeof NavMenuType = NavMenuType;
 
   dreamPlural: SimpleObject = DreamPlural;
+
+  searchForm: FormGroup;
 
   private destroyed$: Subject<void> = new Subject<void>();
 
@@ -98,6 +103,48 @@ export class DiaryComponent implements OnInit, OnDestroy {
     );
   }
 
+  // Данные поиска
+  private get getSearch(): Partial<SearchDream> {
+    const page: number = this.pageCurrent > 0 ? this.pageCurrent : 1;
+    const fromForm: CustomObjectKey<keyof SearchDream, string | number> = Object.entries(this.getDefaultSearch)
+      .filter(([k]) => k !== "page")
+      .reduce((o, [k]) => ({ ...o, [k]: this.searchForm.get(k)?.value?.toString() ?? "" }), {});
+    // Вернуть данные
+    return {
+      ...fromForm,
+      page,
+      user: this.visitedUser?.id ?? 0,
+      status: -1,
+      limit: this.pageLimit
+    };
+  }
+
+  // Текущие данные из URL
+  private get getCurrentSearch(): Partial<SearchDream> {
+    const page: number = parseInt(this.queryParams.page) > 0 ? parseInt(this.queryParams.page) : 1;
+    const fromForm: CustomObjectKey<keyof SearchDream, string | number> = Object.entries(this.getDefaultSearch)
+      .filter(([k]) => k !== "page")
+      .reduce((o, [k]) => ({ ...o, [k]: this.queryParams[k]?.toString() ?? "" }), {});
+    // Вернуть данные
+    return {
+      ...fromForm,
+      page,
+    };
+  }
+
+  // Пустые данные
+  private get getDefaultSearch(): Partial<SearchDream> {
+    return {
+      q: "",
+      page: 1
+    };
+  }
+
+  // Задействован поиск
+  get getIsSearch(): boolean {
+    return !CompareObjects(this.getDefaultSearch, this.getCurrentSearch);
+  }
+
 
 
 
@@ -110,8 +157,14 @@ export class DiaryComponent implements OnInit, OnDestroy {
     private titleService: Title,
     private router: Router,
     private globalService: GlobalService,
+    private formBuilder: FormBuilder,
     private canonicalService: CanonicalService
-  ) { }
+  ) {
+    this.searchForm = this.formBuilder.group(Object.entries(this.getDefaultSearch)
+      .filter(([k]) => k !== "page")
+      .reduce((o, [k, v]) => ({ ...o, [k]: [v, null] }), {})
+    );
+  }
 
   ngOnInit() {
     this.pageData = this.globalService.getPageData;
@@ -152,14 +205,11 @@ export class DiaryComponent implements OnInit, OnDestroy {
     if (this.itsMyPage) {
       this.floatButtonIcon = "add";
       this.floatButtonLink = "/diary/editor";
-      this.canonicalService.setURL("diary/" + this.user.id, { p: this.pageCurrent }, { p: [0, 1] });
       // Заголовок вкладки
       pageTitle = this.globalService.createTitle("Мой дневник сновидений");
     }
     // Дневник другого пользователя
     else if (!this.itsAllPage) {
-      this.canonicalService.setURL("diary/" + this.visitedUser.id, { p: this.pageCurrent }, { p: [0, 1] });
-      // Заголовок вкладки
       pageTitle = this.globalService.createTitle([this.subTitle, this.title]);
     }
     // Общий дневник
@@ -169,7 +219,6 @@ export class DiaryComponent implements OnInit, OnDestroy {
       this.floatButtonIcon = !!this.user ? "add" : "";
       this.floatButtonLink = !!this.user ? "/diary/editor" : "";
       this.menuAvatarIcon = "content_paste_search";
-      this.canonicalService.setURL("diary/all", { p: this.pageCurrent }, { p: [0, 1] });
       // Заголовок вкладки
       pageTitle = this.globalService.createTitle("Общий дневник");
     }
@@ -190,21 +239,19 @@ export class DiaryComponent implements OnInit, OnDestroy {
 
   // Изменение страницы
   onPageChange(event: PaginateEvent): void {
-    const path: string[] = (this.router.url.split("?")[0]).split("/").filter(v => v.length > 0);
-    // Настройки
     this.pageCurrent = event.pageCurrent;
-    // Перейти к новой странице
-    this.router.navigate(path, {
-      queryParams: { ...this.queryParams, p: event.pageCurrent },
-      queryParamsHandling: "merge",
-      replaceUrl: true,
-      state: {
-        changeTitle: false,
-        showPreLoader: false
-      }
-    });
-    // Обновить список
-    this.search();
+    // Изменить URL
+    this.urlSet(this.getSearch);
+  }
+
+  // Событие поиска
+  onSearch(): void {
+    this.urlSet(this.getSearch);
+  }
+
+  // Сбросить поиск
+  onClear(): void {
+    this.urlSet(this.getDefaultSearch);
   }
 
 
@@ -253,10 +300,16 @@ export class DiaryComponent implements OnInit, OnDestroy {
         })
       )
       .subscribe(({ visitedUserId, queryParams }) => {
-        this.pageCurrent = CheckInRange(ParseInt(queryParams?.p), Infinity, 1);
+        this.queryParams = queryParams;
+        this.pageCurrent = CheckInRange(ParseInt(queryParams?.page), Infinity, 1);
         this.visitedUserId = visitedUserId;
         this.itsMyPage = visitedUserId > 0 && visitedUserId === this.user?.id;
         this.itsAllPage = visitedUserId === 0;
+        // Наполнить форму
+        Object.entries(this.getCurrentSearch)
+          .filter(([k]) => k !== "page")
+          .forEach(([k, v]) => this.searchForm.get(k)?.setValue(v));
+        // Обновить
         this.changeDetectorRef.detectChanges();
       });
   }
@@ -310,25 +363,25 @@ export class DiaryComponent implements OnInit, OnDestroy {
   // Определение списка сновидений
   private defineDreamsList(): void {
     this.waitObservable(() => this.visitedUserId === -1 || (this.visitedUserId > 0 && !this.visitedUser))
-      .pipe(takeUntil(this.destroyed$))
-      .subscribe(() => this.search())
+      .pipe(
+        takeUntil(this.destroyed$),
+        concatMap(() => this.activatedRoute.queryParams)
+      )
+      .subscribe(() => this.search());
   }
 
   // Загрузка списка сновидений
   search(): void {
     this.loading = true;
     this.changeDetectorRef.detectChanges();
-    // Поиск по сновидениям
-    const search: SearchDream = {
-      page: this.pageCurrent > 0 ? this.pageCurrent : 1,
-      user: this.visitedUser?.id ?? 0,
-      status: -1
-    };
     // Запрос
-    this.dreamService.search(search, ["0002"])
+    this.dreamService.search(this.getSearch, ["0002"])
       .pipe(takeUntil(this.destroyed$))
       .subscribe(
         ({ result, count, limit, hasAccess }) => {
+          const subPage: string = this.itsMyPage ? this.user.id.toString() : (this.itsAllPage ? "all" : this.visitedUser.id.toString());
+          // Отметка доступа
+          this.canonicalService.setURL("diary/" + subPage, this.getSearch, { p: [0, 1], limit: [0, this.defaultPageLimit] });
           this.userHasAccess = hasAccess;
           // Найдены сновидения
           if (count > 0) {
@@ -353,5 +406,23 @@ export class DiaryComponent implements OnInit, OnDestroy {
   // Показать фильтры
   openSearch(): void {
     this.searchPanel?.openPanel();
+  }
+
+  // Записать параметры в URL
+  private urlSet(datas: Partial<SearchDream>): void {
+    const path: string[] = (this.router.url.split("?")[0]).split("/").filter(v => v.length > 0);
+    const queryParams: CustomObject<string | number> = Object.entries({ ...this.queryParams, ...datas })
+      .map(([k, v]) => ([k, !!v ? v : null]))
+      .reduce((o, [k, v]) => ({ ...o, [k as string]: v }), {});
+    // Перейти к новой странице
+    this.router.navigate(path, {
+      queryParams,
+      queryParamsHandling: "merge",
+      replaceUrl: true,
+      state: {
+        changeTitle: false,
+        showPreLoader: false
+      }
+    });
   }
 }
--- client/src/app/pages/diary/diary.module.ts
+++ client/src/app/pages/diary/diary.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from "@angular/material/tooltip";
 import { DreamListModule } from '@_controlers/dream-list/dream-list.module';
 import { InformModule } from '@_controlers/inform/inform.module';
 import { NavMenuModule } from "@_controlers/nav-menu/nav-menu.module";
 import { PageLoaderModule } from '@_controlers/page-loader/page-loader.module';
 import { PaginationModule } from '@_controlers/pagination/pagination.module';
+import { SearchGroupModule } from "@_controlers/search-group/search-group.module";
 import { SearchPanelModule } from '@_controlers/search-panel/search-panel.module';
+import { TextInputModule } from "@_controlers/text-input/text-input.module";
 import { CoreModule } from '@_modules/core.module';
 import { DiaryRoutingModule } from '@_pages/diary/diary-routing.module';
 import { DiaryComponent } from '@_pages/diary/diary.component';
@@ -27,7 +30,10 @@ import { DiaryComponent } from '@_pages/diary/diary.component';
     InformModule,
     PaginationModule,
     MatButtonModule,
-    SearchPanelModule
+    SearchPanelModule,
+    SearchGroupModule,
+    TextInputModule,
+    MatTooltipModule
   ]
 })
 
