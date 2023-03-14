630ee4cbbd95b6a1d46119e47ac01fc4b89d475e
akb2 <akb2-online@ya.ru>
2023-03-15 02:25:20 +0400

    Поле кому назначен комментарий

--- api/config/mysql_tables/comment/createTable.sql
+++ api/config/mysql_tables/comment/createTable.sql
@@ -2,6 +2,7 @@ CREATE TABLE `comments`
   (
     `id` INT NOT NULL AUTO_INCREMENT,
     `user_id` INT,
+    `reply_to_user_id` INT,
     `material_type` TINYINT(1),
     `material_id` INT,
     `text` TEXT,
@@ -9,6 +10,7 @@ CREATE TABLE `comments`
     `attachment` JSON,
     PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`reply_to_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
     INDEX `user_id` (`user_id`),
     INDEX `create_date` (`create_date`),
     INDEX `material` (`material_type`, `material_id`),
