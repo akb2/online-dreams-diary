2ea66696787cef0d0fd9ff9ec3c1ce0d588c478a
akb2 <akb2-online@ya.ru>
2023-03-17 11:09:23 +0400

    Функция запроса списка комментариев

--- client/src/app/services/comment.service.ts
+++ client/src/app/services/comment.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable, OnDestroy } from "@angular/core";
-import { ObjectToFormData } from "@_datas/api";
+import { ObjectToFormData, ObjectToParams } from "@_datas/api";
 import { ApiResponse } from "@_models/api";
 import { CommentMaterialType } from "@_models/comment";
 import { Observable, Subject, switchMap, takeUntil } from "rxjs";
@@ -37,7 +37,19 @@ export class CommentService implements OnDestroy {
 
   // Отправка комментария
   send(materialType: CommentMaterialType, materialId: number, text: string, codes: string[] = []): Observable<any> {
-    return this.httpClient.post<ApiResponse>("/comment/send", ObjectToFormData({ materialType, materialId, text })).pipe(
+    return this.httpClient.post<ApiResponse>("comment/send", ObjectToFormData({ materialType, materialId, text })).pipe(
+      takeUntil(this.destroyed$),
+      switchMap(data => this.apiService.checkSwitchMap(data, codes))
+    );
+  }
+
+
+
+
+
+  // Получение списка комментариев
+  getList(materialType: CommentMaterialType, materialId: number, codes: string[] = []): Observable<any> {
+    return this.httpClient.get<ApiResponse>("comment/getList", { params: ObjectToParams({ materialType, materialId }) }).pipe(
       takeUntil(this.destroyed$),
       switchMap(data => this.apiService.checkSwitchMap(data, codes))
     );
