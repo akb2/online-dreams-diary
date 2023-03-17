31d875baee7c0073b22f42f1876b3662cf6cf42f
akb2 <akb2-online@ya.ru>
2023-03-17 11:09:38 +0400

    Базовые параметры

--- client/src/app/controlers/lists/comment-list/comment-list.component.ts
+++ client/src/app/controlers/lists/comment-list/comment-list.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from "@angular/core";
 import { Comment } from "@_models/comment";
+import { CommentService } from "@_services/comment.service";
+import { Subject } from "rxjs";
 
 
 
@@ -12,11 +14,30 @@ import { Comment } from "@_models/comment";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class CommentListComponent {
+export class CommentListComponent implements OnInit, OnDestroy {
   @Input() emptyCommentsMainTitle: string = "Нет комментариев";
   @Input() emptyCommentsSubTitle: string = "Будьте первым, напишите свой комментарий";
 
   comments: Comment[] = [];
 
   loading: boolean = false;
+
+  private destroyed$: Subject<void> = new Subject();
+
+
+
+
+
+  constructor(
+    private commentService: CommentService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
 }
