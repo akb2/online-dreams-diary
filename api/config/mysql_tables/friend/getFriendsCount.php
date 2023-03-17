a9b20cd4e69efe8aabf15857c781041646733a20
akb2 <akb2-online@ya.ru>
2023-03-17 09:34:49 +0400

    Обновил запрос

--- api/config/mysql_tables/friend/getFriendsCount.php
+++ api/config/mysql_tables/friend/getFriendsCount.php
@@ -1,10 +1,12 @@
 SELECT COUNT(*)
 FROM `friends`
 WHERE
+`id` > 0
 
 <? if ($input['type'] == 'friends') : ?>
   <?/* // ? Список друзей */ ?>
-  `status` = 1 AND
+  <? $sortField = 'in_date'; ?>
+  AND `status` = 1 AND
   (
   `out_user_id` = :user_id OR
   `in_user_id` = :user_id
@@ -12,25 +14,21 @@ WHERE
 
 <? elseif ($input['type'] == 'subscribers') : ?>
   <?/* // ? Подписчики */ ?>
-  (
-  (
+  AND ((
   `in_user_id` = :user_id AND
   `status` = 0
   ) OR (
   `out_user_id` = :user_id AND
   `status` = 2
-  )
-  )
+  ))
 
 <? elseif ($input['type'] == 'subscribe') : ?>
   <?/* // ? Подписки */ ?>
-  (
-  (
+  AND ((
   `out_user_id` = :user_id AND
   `status` = 0
   ) OR (
   `in_user_id` = :user_id AND
   `status` = 2
-  )
-  )
+  ))
 <? endif; ?>
--- api/config/mysql_tables/friend/getFriendsList.php
+++ api/config/mysql_tables/friend/getFriendsList.php
@@ -7,11 +7,12 @@ DATE_FORMAT(`out_date`, '%Y-%m-%dT%TZ') AS `out_date`,
 DATE_FORMAT(`in_date`, '%Y-%m-%dT%TZ') AS `in_date`
 FROM `friends`
 WHERE
+`id` > 0
 
 <? if ($input['type'] == 'friends') : ?>
   <?/* // ? Список друзей */ ?>
   <? $sortField = 'in_date'; ?>
-  `status` = 1 AND
+  AND `status` = 1 AND
   (
   `out_user_id` = :user_id OR
   `in_user_id` = :user_id
@@ -19,7 +20,7 @@ WHERE
 
 <? elseif ($input['type'] == 'subscribers') : ?>
   <?/* // ? Подписчики */ ?>
-  ((
+  AND ((
   `in_user_id` = :user_id AND
   `status` = 0
   ) OR (
@@ -29,7 +30,7 @@ WHERE
 
 <? elseif ($input['type'] == 'subscribe') : ?>
   <?/* // ? Подписки */ ?>
-  ((
+  AND ((
   `out_user_id` = :user_id AND
   `status` = 0
   ) OR (
