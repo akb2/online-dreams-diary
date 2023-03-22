04f50cf9dd94f16173843c7433ae1551891cfbc4
akb2 <akb2-online@ya.ru>
2023-03-22 04:51:23 +0400

    Функция поднятия первой буквы

--- client/src/app/helpers/string.ts
+++ client/src/app/helpers/string.ts
@@ -15,3 +15,10 @@ export const CreateRandomID = (length: number) => {
     .map(letter => !!Random(0, 1) ? letter.toLowerCase() : letter)
     .join("");
 }
+
+// Сделать первую букву заглавной
+export const CapitalizeFirstLetter = (mixedText: string) => {
+  const text: string = (mixedText ?? "").toLowerCase();
+  // Поднять первую бкуву
+  return text.charAt(0).toUpperCase() + text.slice(1);
+};
