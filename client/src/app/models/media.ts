a285c202e510cd6326c2cfbd414627170b6fcffa
akb2 <akb2-online@ya.ru>
2023-04-09 21:13:43 +0400

    Модель медиа файлов

---
+++ client/src/app/models/media.ts
@@ -0,0 +1,47 @@
+import { User } from "./account";
+
+
+
+
+
+// Данные медиа файла
+export interface MediaFile {
+  id: number;
+  createDate: Date;
+  userId?: number;
+  user: User;
+  hash: string;
+  size: number;
+  extension: MediaFileExtension;
+  originalName: string;
+  keywords: string[];
+  description: string;
+  url: string;
+}
+
+// Данные получаемые с сервера
+export interface MediaFileDto {
+  id: number;
+  createDate: string;
+  userId: number;
+  user?: User;
+  hash: string;
+  size: number;
+  extension: string;
+  originalName: string;
+  keywords: string[];
+  description: string;
+  url: string;
+}
+
+
+
+
+
+// Расширения медиафайлов
+export enum MediaFileExtension {
+  jpg = "jpg",
+  jpeg = "jpeg",
+  png = "png",
+  gif = "gif"
+}
