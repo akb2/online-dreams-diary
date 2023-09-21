ed518198339847c86262bec0117cb4479d17cc1a
akb2 <akb2-online@ya.ru>
2023-09-21 04:57:54 +0400

    Обновил конфигурации для мультидомена

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   baseUrl: {
     default: "https://dreams-diary.ru/",
+    "dreams-diary.su": "https://dreams-diary.su/",
     "dreams-diary.com": "https://dreams-diary.com/",
     "dreams-diary.net": "https://dreams-diary.net/",
     "dreams-diary.org": "https://dreams-diary.org/",
@@ -9,10 +10,6 @@ export const environment = {
   },
   baseApiUrl: {
     default: "https://api.dreams-diary.ru/",
-    "dreams-diary.com": "https://api.dreams-diary.com/",
-    "dreams-diary.net": "https://api.dreams-diary.net/",
-    "dreams-diary.org": "https://api.dreams-diary.org/",
-    "дневник-сновидений.рф": "https://апи.дневник-сновидений.рф/",
   },
   reCaptchaKey: "6LdPdrkjAAAAAKTFoz-shTHQs1XL4edOsYJG-Fo-",
   httpHeader: {},
