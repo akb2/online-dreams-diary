4538f6a604e9203939ec75f445e116b638d39c91
akb2 <akb2-online@ya.ru>
2023-10-29 19:30:31 +0400

    Поддержка переводов во всплывающих уведомлениях

--- client/src/app/services/snackbar.service.ts
+++ client/src/app/services/snackbar.service.ts
@@ -1,6 +1,8 @@
 import { SnackbarProps } from "@_models/app";
-import { Injectable } from "@angular/core";
+import { Injectable, OnDestroy } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { TranslateService } from "@ngx-translate/core";
+import { Subject, takeUntil } from "rxjs";
 
 
 
@@ -10,33 +12,48 @@ import { MatSnackBar } from "@angular/material/snack-bar";
   providedIn: "root"
 })
 
-export class SnackbarService {
+export class SnackbarService implements OnDestroy {
 
 
   private timerForReadASymbol: number = 150;
+
+  private destroyed$: Subject<void> = new Subject();
 
 
 
 
 
   constructor(
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private translateService: TranslateService
   ) { }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
 
 
 
 
 
   // Раскрыть сообщение
-  open({ message, action, mode }: SnackbarProps): void {
-    const actionMsg: string = action ?? "Закрыть";
-    const duration: number = message.length * this.timerForReadASymbol;
-    // Открыть сообщение
-    this.snackBar.open(message, actionMsg, {
-      duration,
-      horizontalPosition: "right",
-      verticalPosition: "bottom",
-      ...(mode && { panelClass: `snackbar-${mode}` })
-    });
+  open({ message: mixedMessage, action, mode }: SnackbarProps): void {
+    const mixedActionMsg: string = action ?? "general.buttons.close";
+    // Загрузка текстов
+    this.translateService.get([mixedMessage, mixedActionMsg])
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(data => {
+        const message: string = data?.[mixedMessage] ?? "";
+        const actionMsg: string = data?.[mixedActionMsg] ?? "";
+        const duration: number = message.length * this.timerForReadASymbol;
+        // Открыть сообщение
+        this.snackBar.open(message, actionMsg, {
+          duration,
+          horizontalPosition: "right",
+          verticalPosition: "bottom",
+          ...(mode && { panelClass: `snackbar-${mode}` })
+        });
+      });
   }
 }
