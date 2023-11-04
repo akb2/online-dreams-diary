c81731383188fc50952fe8879d7b3e032a85877c
akb2 <akb2-online@ya.ru>
2023-11-04 05:52:54 +0400

    Добавил переводы

--- client/src/app/pages/profile-detail/status-block/status-block.component.ts
+++ client/src/app/pages/profile-detail/status-block/status-block.component.ts
@@ -2,11 +2,10 @@ import { ParseInt } from "@_helpers/math";
 import { User } from "@_models/account";
 import { SimpleObject } from "@_models/app";
 import { AccountService } from "@_services/account.service";
-import { ScreenService } from "@_services/screen.service";
-import { AfterContentInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import { AfterViewChecked, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
 import { transform as cssCalc } from "css-calc-transform";
-import { Subject, concatMap, delay, filter, fromEvent, map, merge, skipWhile, takeUntil, takeWhile, timer } from "rxjs";
+import { Subject, fromEvent, skipWhile, takeUntil, takeWhile, timer } from "rxjs";
 
 
 
@@ -19,15 +18,14 @@ import { Subject, concatMap, delay, filter, fromEvent, map, merge, skipWhile, ta
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class StatusBlockComponent implements OnChanges, OnInit, AfterContentInit, OnDestroy {
+export class StatusBlockComponent implements OnChanges, OnInit, AfterViewChecked, OnDestroy {
 
 
   @Input() user: User;
   @Input() itsMyPage: boolean;
 
   @ViewChild("inputField") inputField!: ElementRef;
-  @ViewChild("inputHelper") inputHelper!: ElementRef;
-  @ViewChild("inputHelperText") inputHelperText!: ElementRef;
+  @ViewChild("inputHelperText", { static: false }) inputHelperText!: ElementRef;
   @ViewChild("statusBlock") statusBlock!: ElementRef;
   @ViewChild("statusOverlay") statusOverlay!: ElementRef;
   @ViewChild("saveButton", { read: ElementRef }) saveButton!: ElementRef;
@@ -35,7 +33,6 @@ export class StatusBlockComponent implements OnChanges, OnInit, AfterContentInit
   @ViewChild("editButton", { read: ElementRef }) editButton!: ElementRef;
 
   statusForm: FormGroup;
-  placeholderText: string = "Напишите, что у вас нового...";
 
   loader: boolean = false;
   editHelperStyles: SimpleObject = {};
@@ -66,9 +63,7 @@ export class StatusBlockComponent implements OnChanges, OnInit, AfterContentInit
 
   // Текст для помощника редактора
   get getEditorText(): string {
-    const text: string = this.statusForm?.get('status')?.value;
-    // Вернуть текст
-    return !!text ? text : this.placeholderText;
+    return this.statusForm?.get('status')?.value?.toString() ?? "";
   }
 
   // Убрать отступ справа
@@ -98,8 +93,7 @@ export class StatusBlockComponent implements OnChanges, OnInit, AfterContentInit
   constructor(
     private formBuilder: FormBuilder,
     private changeDetectorRef: ChangeDetectorRef,
-    private accountService: AccountService,
-    private screenService: ScreenService
+    private accountService: AccountService
   ) {
     this.statusForm = this.formBuilder.group({
       status: null
@@ -116,28 +110,15 @@ export class StatusBlockComponent implements OnChanges, OnInit, AfterContentInit
       .subscribe(e => this.onCloseEdit(e as PointerEvent));
   }
 
-  ngAfterContentInit(): void {
-    timer(0, 50)
-      .pipe(
-        takeWhile(() => !this.inputHelperText || !this.statusOverlay || !this.statusForm?.get("status"), true),
-        skipWhile(() => !this.inputHelperText || !this.statusOverlay || !this.statusForm?.get("status")),
-        concatMap(() => merge(
-          this.editStatus$.asObservable(),
-          this.statusForm.get("status").valueChanges.pipe(delay(1)),
-          this.screenService.elmResize([this.statusOverlay.nativeElement])
-        )),
-        map(() => ([this.inputHelperText?.nativeElement])),
-        filter(elms => elms.every(e => !!e)),
-        takeUntil(this.destroyed$)
-      )
-      .subscribe(([helperText]) => {
-        this.editInputStyles = {
-          width: (helperText.getBoundingClientRect().width) + "px",
-          height: (helperText.getBoundingClientRect().height) + "px"
-        };
-        // Обновить
-        this.changeDetectorRef.detectChanges();
-      })
+  ngAfterViewChecked(): void {
+    if (this.inputHelperText?.nativeElement) {
+      this.editInputStyles = {
+        width: (this.inputHelperText.nativeElement.getBoundingClientRect().width) + "px",
+        height: (this.inputHelperText.nativeElement.getBoundingClientRect().height) + "px"
+      };
+      // Обновить
+      this.changeDetectorRef.detectChanges();
+    }
   }
 
   ngOnDestroy(): void {
