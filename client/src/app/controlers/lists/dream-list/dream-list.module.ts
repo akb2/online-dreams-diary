959250d4fc0e4fd20a8371ede58eaf2e5aa565c2
akb2 <akb2-online@ya.ru>
2023-11-09 21:54:38 +0400

    Перевел отметки и дату

--- client/src/app/controlers/lists/dream-list/dream-list.module.ts
+++ client/src/app/controlers/lists/dream-list/dream-list.module.ts
@@ -1,14 +1,15 @@
+import { CardMenuModule } from "@_controlers/card-menu/card-menu.module";
+import { PopupConfirmModule } from "@_controlers/confirm/confirm.module";
+import { DreamListComponent } from "@_controlers/dream-list/dream-list.component";
+import { HighlightKeywordsModule } from "@_controlers/highlight-keywords/highlight-keywordsmodule";
+import { CoreModule } from "@_modules/core.module";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatTooltipModule } from "@angular/material/tooltip";
 import { RouterModule } from "@angular/router";
-import { CardMenuModule } from "@_controlers/card-menu/card-menu.module";
-import { PopupConfirmModule } from "@_controlers/confirm/confirm.module";
-import { DreamListComponent } from "@_controlers/dream-list/dream-list.component";
-import { HighlightKeywordsModule } from "@_controlers/highlight-keywords/highlight-keywordsmodule";
-import { CoreModule } from "@_modules/core.module";
+import { TranslateModule } from "@ngx-translate/core";
 
 
 
@@ -30,7 +31,8 @@ import { CoreModule } from "@_modules/core.module";
     CardMenuModule,
     PopupConfirmModule,
     MatTooltipModule,
-    HighlightKeywordsModule
+    HighlightKeywordsModule,
+    TranslateModule
   ]
 })
 
