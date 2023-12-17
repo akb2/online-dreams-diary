3ca4e315f62bf4729ce27fa371fca0f440331c97
akb2 <akb2-online@ya.ru>
2023-12-17 10:29:07 +0400

    Создание геометрии курсора

--- client/src/app/services/3d/cursor-3d.service.ts
+++ client/src/app/services/3d/cursor-3d.service.ts
@@ -1,6 +1,7 @@
 import { DreamMapTerrainName } from "@_datas/dream-map-objects";
+import { DreamCeilSize } from "@_datas/dream-map-settings";
 import { Injectable } from "@angular/core";
-import { Mesh, ShaderMaterial } from "three";
+import { CylinderGeometry, Mesh, ShaderMaterial } from "three";
 
 
 
@@ -9,8 +10,12 @@ import { Mesh, ShaderMaterial } from "three";
 @Injectable()
 
 export class Cursor3DService {
+  private heightMultiplier = 0.3;
+  private radialSigmentsMultiplier = 8;
+
   mesh: Mesh;
   material: ShaderMaterial;
+  geometry: CylinderGeometry;
 
   hoverItems = [
     DreamMapTerrainName
@@ -20,7 +25,26 @@ export class Cursor3DService {
 
 
 
+  // Получить размер
+  private getGeometryRadius(size: number): number {
+    return (((size * 2) + 1) * DreamCeilSize) / 2;
+  }
+
+
+
+
+
   // Создание курсора
   create(): void {
+    this.createGeometry(1);
+  }
+
+  // Создание геометрии
+  private createGeometry(originalSize: number): void {
+    const radius = this.getGeometryRadius(originalSize);
+    const height = DreamCeilSize * this.heightMultiplier;
+    const radialSegments = originalSize * this.radialSigmentsMultiplier;
+    // Новая геометрия
+    this.geometry = new CylinderGeometry(radius, radius, height, radialSegments, 1, false);
   }
 }
