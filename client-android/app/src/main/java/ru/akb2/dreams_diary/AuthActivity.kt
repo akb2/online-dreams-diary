1dd8ea370513aa24baa4025a91e581c21b9feb44
akb2 <akb2-online@ya.ru>
2024-01-14 21:05:08 +0400

    Создал активность авторизации

---
+++ client-android/app/src/main/java/ru/akb2/dreams_diary/AuthActivity.kt
@@ -0,0 +1,11 @@
+package ru.akb2.dreams_diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
