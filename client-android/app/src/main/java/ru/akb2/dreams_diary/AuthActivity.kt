857b7ffebab816adcbe98d7836df754a3d42f341
akb2 <akb2-online@ya.ru>
2024-01-15 05:02:29 +0400

    Стилизовал нижнюю панель

--- client-android/app/src/main/java/ru/akb2/dreams_diary/AuthActivity.kt
+++ client-android/app/src/main/java/ru/akb2/dreams_diary/AuthActivity.kt
@@ -1,6 +1,7 @@
 package ru.akb2.dreams_diary
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -8,5 +9,6 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
     }
 }
