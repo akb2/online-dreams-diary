5b32722c826b06f417cba7b8bc669e6fae79ad23
akb2 <akb2-online@ya.ru>
2025-03-06 20:45:44 +0300

    Обновил

--- client-android/app/build.gradle.kts
+++ client-android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "ru.akb2.dreams_diary"
-    compileSdk = 34
+    compileSdk = 35
     flavorDimensions += "environment"
 
     defaultConfig {
@@ -59,17 +59,18 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("io.ktor:ktor-client-core:3.0.0-beta-1")
     implementation("io.ktor:ktor-client-okhttp:3.0.0-beta-1")
     implementation("io.ktor:ktor-client-serialization:3.0.0-beta-1")
     implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.0-beta-1")
     implementation("io.ktor:ktor-client-content-negotiation:3.0.0-beta-1")
+    implementation( "androidx.security:security-crypto:1.1.0-alpha06")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
