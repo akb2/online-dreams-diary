0048b7ed34e6242ca6e031fcf01812c47683dec1
akb2 <akb2-online@ya.ru>
2025-03-09 21:20:28 +0300

    Добавил имя пользователя в заголовок

--- client-android/app/src/main/java/ru/akb2/dreams_diary/activities/DiaryActivity.kt
+++ client-android/app/src/main/java/ru/akb2/dreams_diary/activities/DiaryActivity.kt
@@ -2,8 +2,11 @@ package ru.akb2.dreams_diary.activities
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import ru.akb2.dreams_diary.R
+import ru.akb2.dreams_diary.components.MenuToolbar
 import ru.akb2.dreams_diary.datas.AuthType
 import ru.akb2.dreams_diary.store.view_model.UserViewModel
 
@@ -13,22 +16,40 @@ class DiaryActivity : BaseActivity() {
 
     private val userViewModel: UserViewModel by viewModels()
 
+    private lateinit var toolbar: MenuToolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_diary)
         // Настройка активности
         setDarkNavigationIconsColor()
         fillData()
+        userDataListen()
     }
 
     /**
      * Заполнение свойств класса
      * */
     private fun fillData() {
+        toolbar = findViewById(R.id.toolbar)
         mainLayout = findViewById(R.id.mainLayout)
     }
 
     /**
      * Прослушивание данных о пользователе
      */
+    private fun userDataListen() {
+        lifecycleScope.launch {
+            userViewModel.state.collect { state ->
+                val title = if (state.isLoading)
+                    getString(R.string.loading)
+                else if (state.user !== null)
+                    "${state.user.name} ${state.user.lastName}"
+                else
+                    getString(R.string.user_no_name)
+
+                toolbar.setTitle(title)
+            }
+        }
+    }
 }
